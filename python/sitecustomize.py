06757e924307db66abedce8a3bcce6cf6d2875d7
stuart <none@none>
2006-10-10 16:30:02 -0600

    It wasn't actually setting the default encoding. Fixed.

--- python/sitecustomize.py
+++ python/sitecustomize.py
@@ -1,7 +1,10 @@
 # Enable to support locale aware default string encodings.
 # This code copied from Python's site.py.
 
-import locale
+print "executing syscustomize"
+import sys,locale
 loc = locale.getdefaultlocale()
-if loc[1]: encoding = loc[1]
-print "syscustomize executed"
+if loc[1]: 
+    encoding = loc[1]
+    print "setting default encoding to",encoding
+    sys.setdefaultencoding(encoding)

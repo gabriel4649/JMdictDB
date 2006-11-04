8ad88c0778f06748158b0d3daf72f127bd452fe4
stuart <none@none>
2006-11-03 23:50:55 -0700

    Accept option string as commnd argument. Fix the frequency-of-use input stuff so that it is functional   now. Allow display of search sql on results page for debugging. Fix problem where searching for two glosses would never find   anything because the generated sql asked for rows where the   same gloss was simulataneously equal to g1 and g2.  Now, g1   and g2 need only occur in the same sense.  (We probably   want "in the same entry" iunstead, but that will need to   wait.)  Same problem also occured (and ws fixed) for kanji   and reading searches too.

--- python/www/srchres.py
+++ python/www/srchres.py
@@ -12,7 +12,7 @@ _AUTHOR_ = "Stuart McGraw <smcgraw@frii.com>"
 # and &q=nnnn will display the entry with ent_seq nnnn.
 # You can give as many &e= and &q= parameters as you wish.
 
-import sys, os, cgi, cgitb
+import sys, os, re, cgi, cgitb
 sys.path.append (r"c:\stuart\develop\jdb\jb")
 import db, jbdb
 from tables import *
@@ -25,12 +25,16 @@ global KW
 def main (args, opts):
 	global KW
 
-	#sys.stdout = BufferedOutput (sys.stdout)
-	if len(args)==0 or args[0]=="": cgitb.enable()
-
-	try: pt = os.environ['PATH_TRANSLATED'].replace('\\', '/')
-	except KeyError: base = "."
-	else: base = pt[:pt.rfind('/')]
+	if len(args)==0 or args[0]=="": 
+	    cgitb.enable()
+	    try: pt = os.environ['PATH_TRANSLATED'].replace('\\', '/')
+	    except KeyError: base = "."
+	    else: base = pt[:pt.rfind('/')]
+	    env = os.environ
+	else:
+	    env = dict()
+	    env["QUERY_STRING"] = args[0]
+	    base = "."
 
 	# open the database...
 	cursor = db.dbOpen (user="postgres", password="satomi", database="jb")
@@ -39,63 +43,93 @@ def main (args, opts):
 	KW = jbdb.Kwds (cursor)
 
 	t = [None,None,None]; s = [None,None,None]; y = [None,None,None]  
-	if len(args)==0 or args[0]=="":
-	    frm = cgi.FieldStorage()
-	    s[0]=frm.getvalue("s1"); y[0]=frm.getvalue("y1"); t[0]=frm.getvalue("t1")
-	    s[1]=frm.getvalue("s2"); y[1]=frm.getvalue("y2"); t[1]=frm.getvalue("t2")
-	    s[2]=frm.getvalue("s3"); y[2]=frm.getvalue("y3"); t[2]=frm.getvalue("t3")
-	    pos=frm.getlist("pos");  misc=frm.getlist("misc")
-	    src=frm.getlist("src");  freq=frm.getlist("freq")
-	    nfval=frm.getvalue("nfval"); nfcmp=frm.getvalue("nfcmp")
+	frm = cgi.FieldStorage(environ=env)
+	s[0]=frm.getvalue("s1"); y[0]=frm.getvalue("y1"); t[0]=frm.getvalue("t1")
+	s[1]=frm.getvalue("s2"); y[1]=frm.getvalue("y2"); t[1]=frm.getvalue("t2")
+	s[2]=frm.getvalue("s3"); y[2]=frm.getvalue("y3"); t[2]=frm.getvalue("t3")
+	pos=frm.getlist("pos");  misc=frm.getlist("misc")
+	src=frm.getlist("src");  freq=frm.getlist("freq")
+	nfval=frm.getvalue("nfval"); nfcmp=frm.getvalue("nfcmp")
 
-	    idval=frm.getvalue("idval"); idtbl=frm.getvalue("idtyp")
+	idval=frm.getvalue("idval"); idtbl=frm.getvalue("idtyp")
 
-	    if idval: 
-		if idtbl != "seqnum":  col = "id"
-		else: idtbl, col = "entr", "seq"
-		sql, sql_args = jbdb.build_search_sql (
+	if idval: 
+	    if idtbl != "seqnum":  col = "id"
+	    else: idtbl, col = "entr", "seq"
+	    sql, sql_args = jbdb.build_search_sql (
 			[(idtbl, "%s.%s=%%s" % (idtbl,col), (idval,))])
-	    else: 
-		condlist = []
-		for i in 0,1,2:
-	            if t[i]: condlist.append (str_match_clause (s[i],y[i],t[i].decode("utf-8")))
-		if pos: condlist.append (("pos",getsel("pos",pos),()))
-		if misc: condlist.append (("misc",getsel("misc",misc),()))
-		if src: condlist.append (("src",getsel("src",src),()))
-		sql, sql_args = jbdb.build_search_sql (condlist)
+	else: 
+	    condlist = []
+	    for i in 0,1,2:
+		if t[i]: condlist.append (str_match_clause (s[i],y[i],t[i].decode("utf-8"),i))
+	    if pos: condlist.append (("pos",getsel("pos.kw",pos),()))
+	    if misc: condlist.append (("misc",getsel("misc.kw",misc),()))
+	    if src: condlist.append (("entr",getsel("entr.src",src),()))
+	    if freq: condlist.extend (freq_srch_clause (freq, nfval, nfcmp))
+	    sql, sql_args = jbdb.build_search_sql (condlist)
 
 	sql = "SELECT q.* FROM entr_summary q JOIN (%s) AS i ON i.id=q.id" % sql
+	#print "Content-type: text/html\n\n%s\n%s" % (sql,str(sql_args))
 	cursor.execute (sql, sql_args)
 	rs = cursor.fetchall ()
 	if len(rs) == 1:
 	    print "Location: entr.py?e=%d\n" % rs[0][0]
 	else:
 	    print "Content-type: text/html\n"
-	    display_entries (base, rs)
+	    display_entries (base, rs, sql)
 	
-def display_entries (base, rs):
+def display_entries (base, rs, sql=None):
 	tmpl = mktemplate(base+"/templates/srchres.tal")
-	serialize (tmpl, sys.stdout, results=rs)
+	serialize (tmpl, sys.stdout, results=rs, sql=sql)
 
-def str_match_clause (srchin, srchtyp, srchfor):
+def freq_srch_clause (freq, nfval, nfcmp):
+	x = {}; whr = []
+	for f in freq:
+	    domain,value = re.findall("^(\D+)(\d*)$", f)[0]
+	    if domain == "nf": continue
+	    x.setdefault(domain,[]).append(value)
+	for k,v in x.items():
+	    kwid = KW.FREQ[k].id
+	    if len(v)==2 or k=="spec": whr.append (
+		"(kfreq.kw=%s OR rfreq.kw=%s)" % (kwid,kwid))
+		# Above assumes only values possible are 1 and 2.
+	    elif len(v) == 1: whr.append (
+		"((kfreq.kw=%s AND kfreq.value=%s) OR (rfreq.kw=%s AND rfreq.value=%s))" 
+		% (kwid, v[0], kwid, v[0]))
+	    elif len(v) > 2: whr.append (
+		"((kfreq.kw=%s AND kfreq.value IN (%s)) OR (rfreq.kw=%s AND rfreq.value IN (%s)))" 
+		% (k, ",".join(v), k, ",".join(v)))
+	    else: raise ValueError
+	if "nf" in freq and nfval is not None:
+	    kwid = KW.FREQ['nf'].id
+	    whr.append ("((kfreq.kw=%s AND kfreq.value%s%s)" 
+			" OR (rfreq.kw=%s AND rfreq.value%s%s))"
+		% (kwid, nfcmp, nfval,  kwid, nfcmp, nfval))
+	whr = "(" + " OR ".join(whr) + ")"
+	if not whr: return ()
+	return ("*rfreq","",()),("*kfreq",whr,())
+
+def str_match_clause (srchin, srchtyp, srchfor, idx):
 	if srchin == "auto":
 	    x = jbdb.jstr_classify (srchfor)
 	    if x & jbdb.KANJI: table = "kanj"
 	    elif x & jbdb.KANA: table = "kana"
 	    else: table = "gloss"
 	else: table = srchin
+	alias = {"kanj":"j", "kana":"r", "gloss":"g"}[table] + str(idx)
 	srchtyp = srchtyp.lower()
-	if srchtyp == "is":         whr = "%s.txt=%%s" % table
-	else:                       whr = "%s.txt LIKE(%%s)" % table
+	if srchtyp == "is":         whr = "%s.txt=%%s" % alias
+	else:                       whr = "%s.txt LIKE(%%s)" % alias
 	if srchtyp == "is":         args = (srchfor,) 
 	elif srchtyp == "starts":   args = (srchfor+"%",)
 	elif srchtyp == "contains": args = ("%"+srchfor+"%",)
 	elif srchtyp == "ends":     args = ("%"+srchfor,)
 	else: raise ValueError ("srchtyp = %s" % srchtyp)
-	return table,whr,args
+	return "%s %s" % (table,alias),whr,args
 
-def getsel (name, itms):
-	s = "%s IN (%s)" % (name, ",".join([str(x) for x in itms]))
+def getsel (fqcol, itms):
+	s = "%s IN (%s)" % (fqcol, ",".join([str(x) for x in itms]))
+	#s = " AND ".join(["%s=%s" % (fqcol,x) for x in itms])
 	return s
 
 #---------------------------------------------------------------------

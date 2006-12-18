bb495c080284def74a48e0e061293961e4b8fe87
stuart <none@none>
2006-12-18 11:54:22 -0700

    Drop old database before setiing "stop on error" so that procedure will run even first time when old database does not exist.

--- pg/reload.sql
+++ pg/reload.sql
@@ -1,6 +1,15 @@
-\set ON_ERROR_STOP 1
+-- Run this file from a shell cd'd to the parent
+-- directory of the directory this file is in
+-- using a command like:
+-- 
+--    psql -f pg/load.sql -d postgres [-U username]
+--
+-- You may need to use additional arguments such
+-- as '-U username' depending on exiting defaults.
+
 \c postgres
 drop database jb;
+\set ON_ERROR_STOP 1
 create database jb encoding 'utf8';
 \c jb
 \i pg/schema.sql

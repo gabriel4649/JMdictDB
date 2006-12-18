a678593a302da0130c2af575c6b4605fdc4a2c2a
stuart <none@none>
2006-12-18 12:03:15 -0700

    Duplicate 'uK' keyword needed for 2006-12-17 jmdict file.  Is also in MISC. Change id numbers of the kanjdict keywords to allow contiguous numbering for future  jmdict keyword additions.

--- pg/data/kwrinf.sql
+++ pg/data/kwrinf.sql
@@ -4,8 +4,9 @@ INSERT INTO kwrinf(id,kw,descr) VALUES(3,  'ik',     'word containing irregular 
 INSERT INTO kwrinf(id,kw,descr) VALUES(4,  'ok',     'out-dated or obsolete kana usage');
 INSERT INTO kwrinf(id,kw,descr) VALUES(5,  'rare',   'rare');
 INSERT INTO kwrinf(id,kw,descr) VALUES(6,  'uk',     'word usually written using kana alone');
-INSERT INTO kwrinf(id,kw,descr) VALUES(7,  'kun',    'japanese reading (kun-yomi)');
-INSERT INTO kwrinf(id,kw,descr) VALUES(8,  'on',     'chinese reading (on-yomi)');
-INSERT INTO kwrinf(id,kw,descr) VALUES(9,  'name',   'reading used only in names (nanori)');
-INSERT INTO kwrinf(id,kw,descr) VALUES(10, 'rad',    'reading used as name of radical');
-INSERT INTO kwrinf(id,kw,descr) VALUES(11, 'jouyou', 'Approved reading for jouyou kanji');
+INSERT INTO kwrinf(id,kw,descr) VALUES(7,  'uK',     'word usually written using kanji alone');
+INSERT INTO kwrinf(id,kw,descr) VALUES(21,  'kun',    'japanese reading (kun-yomi)');
+INSERT INTO kwrinf(id,kw,descr) VALUES(22,  'on',     'chinese reading (on-yomi)');
+INSERT INTO kwrinf(id,kw,descr) VALUES(23,  'name',   'reading used only in names (nanori)');
+INSERT INTO kwrinf(id,kw,descr) VALUES(24, 'rad',    'reading used as name of radical');
+INSERT INTO kwrinf(id,kw,descr) VALUES(25, 'jouyou', 'Approved reading for jouyou kanji');

854fbeb6287ad4d673f3a131d6cdaa92763205a2
stuart <none@none>
2006-12-18 11:58:51 -0700

    Update description of thb dialect. Add tsug dialect.

--- pg/data/kwdial.sql
+++ pg/data/kwdial.sql
@@ -4,4 +4,6 @@ INSERT INTO kwdial(id,kw,descr) VALUES(3,'ktb','Kantou-ben');
 INSERT INTO kwdial(id,kw,descr) VALUES(4,'kyb','Kyoto-ben');
 INSERT INTO kwdial(id,kw,descr) VALUES(5,'osb','Osaka-ben');
 INSERT INTO kwdial(id,kw,descr) VALUES(6,'tsb','Tosa-ben');
-INSERT INTO kwdial(id,kw,descr) VALUES(7,'thb','A dialect');
+INSERT INTO kwdial(id,kw,descr) VALUES(7,'thb','Hokaido-ben');
+INSERT INTO kwdial(id,kw,descr) VALUES(8,'tsug','Tsugaru-ben');
+

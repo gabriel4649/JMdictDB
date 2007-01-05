7f5676e4e927a6105b9421b6158dfed2fa60da92
stuart <none@none>
2007-01-04 23:20:16 -0700

    Moved jmdict.pm from directory perl/ to perl/lib/ (in CVS   repository). Change some hash key names in the structure returned by   EntrList to be more consistent with table/column names. In KW structure, use same 1st level key (e.g. 'POS') to   hold both 'kw' hash and 'id' hash.  Formerly the latter   used a key prefixed with "i" (e.g. 'iPOS'). Move sub filt() from showentr.pl to here. Add sub's irestr(), istagr(), istagk() which will convert   the restriction lists from the database form to jmdict form.

--- perl/lib/jmdict.pm
+++ perl/lib/jmdict.pm
@@ -1,8 +1,10 @@
-# Copyright (c) 2006, Stuart McGraw 
+package jmdict;
+# Copyright (c) 2006,2007 Stuart McGraw 
 @VERSION = (substr('$Revision$',11,-2), \
 	    substr('$Date$',7,-11));
 
-use strict;
+package main;
+use strict; use warnings;
 
     sub dbread { my ($dbh, $sql, $args) = @_;
 	my ($sth, @rs, $r);
@@ -16,25 +18,25 @@ use strict;
     sub Kwds { my ($dbh) = @_;
 	my (%kw);
 	$kw{DIAL} = $dbh->selectall_hashref("SELECT * FROM kwdial", "kw");
-	$kw{iDIAL} = $dbh->selectall_hashref("SELECT * FROM kwdial", "id");
-	$kw{FLD} = $dbh->selectall_hashref("SELECT * FROM kwfld", "kw");
-	$kw{iFLD} = $dbh->selectall_hashref("SELECT * FROM kwfld", "id");
+	$kw{DIAL} = $dbh->selectall_hashref("SELECT * FROM kwdial", "id");
+	$kw{FLD}  = $dbh->selectall_hashref("SELECT * FROM kwfld",  "kw");
+	$kw{FLD}  = $dbh->selectall_hashref("SELECT * FROM kwfld",  "id");
 	$kw{FREQ} = $dbh->selectall_hashref("SELECT * FROM kwfreq", "kw");
-	$kw{iFREQ} = $dbh->selectall_hashref("SELECT * FROM kwfreq", "id");
+	$kw{FREQ} = $dbh->selectall_hashref("SELECT * FROM kwfreq", "id");
 	$kw{KINF} = $dbh->selectall_hashref("SELECT * FROM kwkinf", "kw");
-	$kw{iKINF} = $dbh->selectall_hashref("SELECT * FROM kwkinf", "id");
+	$kw{KINF} = $dbh->selectall_hashref("SELECT * FROM kwkinf", "id");
 	$kw{LANG} = $dbh->selectall_hashref("SELECT * FROM kwlang", "kw");
-	$kw{iLANG} = $dbh->selectall_hashref("SELECT * FROM kwlang", "id");
+	$kw{LANG} = $dbh->selectall_hashref("SELECT * FROM kwlang", "id");
 	$kw{MISC} = $dbh->selectall_hashref("SELECT * FROM kwmisc", "kw");
-	$kw{iMISC} = $dbh->selectall_hashref("SELECT * FROM kwmisc", "id");
-	$kw{POS} = $dbh->selectall_hashref("SELECT * FROM kwpos", "kw");
-	$kw{iPOS} = $dbh->selectall_hashref("SELECT * FROM kwpos", "id");
+	$kw{MISC} = $dbh->selectall_hashref("SELECT * FROM kwmisc", "id");
+	$kw{POS}  = $dbh->selectall_hashref("SELECT * FROM kwpos",  "kw");
+	$kw{POS}  = $dbh->selectall_hashref("SELECT * FROM kwpos",  "id");
 	$kw{RINF} = $dbh->selectall_hashref("SELECT * FROM kwrinf", "kw");
-	$kw{iRINF} = $dbh->selectall_hashref("SELECT * FROM kwrinf", "id");
-	$kw{SRC} = $dbh->selectall_hashref("SELECT * FROM kwsrc", "kw");
-	$kw{iSRC} = $dbh->selectall_hashref("SELECT * FROM kwsrc", "id");
+	$kw{RINF} = $dbh->selectall_hashref("SELECT * FROM kwrinf", "id");
+	$kw{SRC}  = $dbh->selectall_hashref("SELECT * FROM kwsrc",  "kw");
+	$kw{SRC}  = $dbh->selectall_hashref("SELECT * FROM kwsrc",  "id");
 	$kw{XREF} = $dbh->selectall_hashref("SELECT * FROM kwxref", "kw");
-	$kw{iXREF} = $dbh->selectall_hashref("SELECT * FROM kwxref", "id");
+	$kw{XREF} = $dbh->selectall_hashref("SELECT * FROM kwxref", "id");
 	return \%kw; }
 
     sub mktmptbl { my ($dbh) = @_;
@@ -66,48 +68,48 @@ use strict;
 	    $com = "FROM entr e JOIN $cond t ON t.id=e.id ";
 	    $eord = "ORDER BY t.ord" }
 
-	my $entr = dbread ($dbh, "SELECT e.* $com $where $eord", $args);
-	my $dial = dbread ($dbh, "SELECT x.* $com JOIN dial x ON x.entr=e.id $where;", $args);
-	my $lang = dbread ($dbh, "SELECT x.* $com JOIN lang x ON x.entr=e.id $where;", $args);
-	my $hist = dbread ($dbh, "SELECT x.* $com JOIN hist x ON x.entr=e.id $where;", $args);
-	my $rdng = dbread ($dbh, "SELECT r.* $com JOIN rdng r ON r.entr=e.id $where ORDER BY r.entr,r.ord;", $args);
-	my $rfrq = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN rfreq x ON x.rdng=r.id $where;", $args);
-	my $rinf = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN rinf  x ON x.rdng=r.id $where;", $args);
-	my $audi = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN audio x ON x.rdng=r.id $where;", $args);
-	my $kanj = dbread ($dbh, "SELECT k.* $com JOIN kanj k ON k.entr=e.id $where ORDER BY k.entr,k.ord;", $args);
-	my $kfrq = dbread ($dbh, "SELECT x.* $com JOIN kanj k ON k.entr=e.id JOIN kfreq x ON x.kanj=k.id $where;", $args);
-	my $kinf = dbread ($dbh, "SELECT x.* $com JOIN kanj k ON k.entr=e.id JOIN kinf  x ON x.kanj=k.id $where;", $args);
-	my $sens = dbread ($dbh, "SELECT s.* $com JOIN sens s ON s.entr=e.id $where ORDER BY s.entr,s.ord;", $args);
-	my $glos = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN gloss x ON x.sens=s.id $where ORDER BY x.sens,x.ord;", $args);
-	my $misc = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN misc  x ON x.sens=s.id $where;", $args);
-	my $pos  = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN pos   x ON x.sens=s.id $where;", $args);
-	my $fld  = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN fld   x ON x.sens=s.id $where;", $args);
+	my $entr  = dbread ($dbh, "SELECT e.* $com $where $eord", $args);
+	my $dial  = dbread ($dbh, "SELECT x.* $com JOIN dial x ON x.entr=e.id $where;", $args);
+	my $lang  = dbread ($dbh, "SELECT x.* $com JOIN lang x ON x.entr=e.id $where;", $args);
+	my $hist  = dbread ($dbh, "SELECT x.* $com JOIN hist x ON x.entr=e.id $where;", $args);
+	my $rdng  = dbread ($dbh, "SELECT r.* $com JOIN rdng r ON r.entr=e.id $where ORDER BY r.entr,r.ord;", $args);
+	my $rfreq = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN rfreq x ON x.rdng=r.id $where;", $args);
+	my $rinf  = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN rinf  x ON x.rdng=r.id $where;", $args);
+	my $audio = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN audio x ON x.rdng=r.id $where;", $args);
+	my $kanj  = dbread ($dbh, "SELECT k.* $com JOIN kanj k ON k.entr=e.id $where ORDER BY k.entr,k.ord;", $args);
+	my $kfreq = dbread ($dbh, "SELECT x.* $com JOIN kanj k ON k.entr=e.id JOIN kfreq x ON x.kanj=k.id $where;", $args);
+	my $kinf  = dbread ($dbh, "SELECT x.* $com JOIN kanj k ON k.entr=e.id JOIN kinf  x ON x.kanj=k.id $where;", $args);
+	my $sens  = dbread ($dbh, "SELECT s.* $com JOIN sens s ON s.entr=e.id $where ORDER BY s.entr,s.ord;", $args);
+	my $gloss = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN gloss x ON x.sens=s.id $where ORDER BY x.sens,x.ord;", $args);
+	my $misc  = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN misc  x ON x.sens=s.id $where;", $args);
+	my $pos   = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN pos   x ON x.sens=s.id $where;", $args);
+	my $fld   = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN fld   x ON x.sens=s.id $where;", $args);
 	my $restr = dbread ($dbh, "SELECT x.* $com JOIN rdng r ON r.entr=e.id JOIN restr x ON x.rdng=r.id $where;", $args);
 	my $stagr = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN stagr x ON x.sens=s.id $where;", $args);
 	my $stagk = dbread ($dbh, "SELECT x.* $com JOIN sens s ON s.entr=e.id JOIN stagk x ON x.sens=s.id $where;", $args);
-	my $xref = dbread ($dbh, "SELECT x.sens,x.typ,z.id,z.seq,z.txt $com JOIN sens s ON s.entr=e.id JOIN xref x ON x.sens=s.id JOIN sref z ON z.sid=x.xref GROUP BY x.sens,x.typ,z.id,z.seq,z.txt ORDER BY x.sens,x.typ $where;", $args);
+	my $xref  = dbread ($dbh, "SELECT x.sens,x.typ,z.id,z.seq,z.txt $com JOIN sens s ON s.entr=e.id JOIN xref x ON x.sens=s.id JOIN sref z ON z.sid=x.xref GROUP BY x.sens,x.typ,z.id,z.seq,z.txt ORDER BY x.sens,x.typ $where;", $args);
+	my $xrer  = dbread ($dbh, "SELECT x.xref,x.typ,z.id,z.seq,z.txt $com JOIN sens s ON s.entr=e.id JOIN xref x ON x.xref=s.id JOIN sref z ON z.sid=x.sens GROUP BY x.xref,x.typ,z.id,z.seq,z.txt ORDER BY x.xref,x.typ $where;", $args);
 
-	matchup ($entr, "_dial", $dial, "entr");
-	matchup ($entr, "_lang", $lang, "entr");
-	matchup ($entr, "_rdng", $rdng, "entr");
-	matchup ($entr, "_kanj", $kanj, "entr");
-	matchup ($entr, "_sens", $sens, "entr");
-	matchup ($entr, "_hist", $hist, "entr");
-	matchup ($rdng, "_rfrq", $rfrq, "rdng");
-	matchup ($rdng, "_rinf", $rinf, "rdng");
-	matchup ($rdng, "_audi", $audi, "rdng");
-	matchup ($kanj, "_kfrq", $kfrq, "kanj");
-	matchup ($kanj, "_kinf", $kinf, "kanj");
-	matchup ($sens, "_glos", $glos, "sens");
-	matchup ($sens, "_pos",  $pos,  "sens");
-	matchup ($sens, "_misc", $misc, "sens");
-	matchup ($sens, "_fld",  $fld,  "sens");
+	matchup ($entr, "_dial",  $dial,  "entr");
+	matchup ($entr, "_lang",  $lang,  "entr");
+	matchup ($entr, "_rdng",  $rdng,  "entr");
+	matchup ($entr, "_kanj",  $kanj,  "entr");
+	matchup ($entr, "_sens",  $sens,  "entr");
+	matchup ($entr, "_hist",  $hist,  "entr");
+	matchup ($rdng, "_rfreq", $rfreq, "rdng");
+	matchup ($rdng, "_rinf",  $rinf,  "rdng");
+	matchup ($rdng, "_audio", $audio, "rdng");
+	matchup ($kanj, "_kfreq", $kfreq, "kanj");
+	matchup ($kanj, "_kinf",  $kinf,  "kanj");
+	matchup ($sens, "_gloss", $gloss, "sens");
+	matchup ($sens, "_pos",   $pos,   "sens");
+	matchup ($sens, "_misc",  $misc,  "sens");
+	matchup ($sens, "_fld",   $fld,   "sens");
 	matchup ($rdng, "_restr", $restr, "rdng");
 	matchup ($sens, "_stagr", $stagr, "sens");
 	matchup ($sens, "_stagk", $stagk, "sens");
-	matchup ($sens, "_xref", $xref, "sens");
-	#matchup ($sens, "_xrer", $xrer, "xref");
-
+	matchup ($sens, "_xref",  $xref,  "sens");
+	matchup ($sens, "_xrer",  $xrer,  "xref");
 	return $entr; }
 
     sub matchup { my ($parents, $attrname, $children, $fkname) = @_;
@@ -121,5 +123,51 @@ use strict;
 		    push (@{$p->{$attrname}}, $c);
 		    last; } }
 	    if (!$found) { die ("Parent not found, fk=$fk\n"); } } }
+
+    sub irestr { my ($entrs) = @_; 
+	my ($e, $r, $nkanj, $rk, $restrs);
+	foreach $e (@$entrs) {
+	    $restrs = 0;
+	    $nkanj = $e->{_kanj} ? scalar (@{$e->{_kanj}}) : 0;
+	    foreach $r (@{$e->{_rdng}}) {
+		if (!$r->{_restr}) { $r->{_restr} = []; next; } # All kanji ok.
+		$restrs = 1;
+		if (scalar (@{$r->{_restr}}) == $nkanj) { 
+		    $r->{_restr} = 1; next; } # nokanji
+		$rk = filt ($e->{_kanj}, $r->{_restr}, "kanj"); 
+		$r->{_restr} = $rk; }
+	    $e->{_restr} = $restrs; } }
+
+    sub istagr { my ($entrs) = @_; 
+	foreach my $e (@$entrs) {
+	    foreach my $s (@{$e->{_sens}}) {
+		if ($s->{_stagr}) { $s->{_stagr} = filt ($e->{_rdng}, $s->{_stagr}, "rdng"); } } } }
+
+    sub istagk { my ($entrs) = @_; 
+	foreach my $e (@$entrs) {
+	    foreach my $s (@{$e->{_sens}}) {
+		if ($s->{_stagk}) { $s->{_stagk} = filt ($e->{_kanj}, $s->{_stagk}, "kanj"); } } } }
+
+    sub filt { my ($targ, $restr, $attrnm) = @_;
+	# Return a list of all elements in @$targ except those that have
+	# an {id} value that is equal to any {attrnm} value in the list 
+	# @$restr.
+	#
+	# Parameters:
+	#   $targ -- Ref to array table rows.
+	#   $restr -- Ref to array of restr rows.
+	#   $attrnm -- Name of attribute in @$restr
+	#     that contains id's matching id's in
+	#     @$targ.
+
+	my ($t, $r, @results, $found);
+	foreach $t (@$targ) {
+	    $found = 0;
+	    foreach $r (@$restr) {
+		if ($t->{id} == $r->{$attrnm}) {
+		    $found = 1;
+		    last; } }
+	    push (@results, $t) if (!$found); }
+	return \@results; }
 
     1;

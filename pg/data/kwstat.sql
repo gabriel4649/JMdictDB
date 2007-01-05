04cf7ab99ac4e17ff65c5c861f52e172ffa6e5ae
stuart <none@none>
2007-01-05 15:04:42 -0700

    Change the "stat" column in tables "entr" and "hist" from   a checked character to a keyword integer that is a foreign   key to new table "kwstat".

---
+++ pg/data/kwstat.sql
@@ -0,0 +1,8 @@
+INSERT INTO kwstat(id,kw,descr) VALUES(1,'N','New entry, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(2,'A','Active entry');
+INSERT INTO kwstat(id,kw,descr) VALUES(3,'M','Modified entry, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(4,'X','Deleted entry');
+INSERT INTO kwstat(id,kw,descr) VALUES(5,'D','Deleted entry, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(6,'O','Obsoleted entry');
+INSERT INTO kwstat(id,kw,descr) VALUES(8,'R','Rejected entry');
+

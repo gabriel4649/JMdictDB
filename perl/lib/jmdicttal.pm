2001dbc178ee4172c32d7e4892edba1635677133
stuart <none@none>
2007-01-13 22:29:53 -0700

    Steal the split_args() and fetch_arg() subs from Petal::Utils and   rewrite our modifiers to use them. Add new modifiers "gt", "lt", "len", and (untested) "perl". Change "freqs" modifier to not require first argument "FREQ".

--- perl/lib/jmdicttal.pm
+++ perl/lib/jmdicttal.pm
@@ -8,42 +8,77 @@ package jmdicttal;
 
 use strict;  use warnings;
 
-$Petal::Hash::MODIFIERS->{'kwabbr:'} = sub { my ($hash, $args) = @_;
-	my ($typ, $expr, $x, $a);
-	($typ, $expr) = split (/ /, $args);
-	$x = $hash->fetch ($expr);
-	$a = $::KW->{$typ}{$x}{kw};
+# split_args() and fetch_arg() are borrowed from Petal::Utils:::Base.
+
+sub split_args { my ($args) = @_;
+	return ($args =~ /('[^']+'|\S+)/g); }
+
+sub fetch_arg { my ($hash, $arg) = @_;
+	return undef unless defined($arg);
+	if ($arg =~ /\'/) { $arg =~ s/\'//g; return $arg; }
+	elsif ($arg =~ /^[0-9.]+$/) { return $arg; }
+	else { return $hash->fetch($arg); } }
+
+$Petal::Hash::MODIFIERS->{'perl:'} = sub { my ($hash, $args) = @_;
+	my $a = eval $args;
 	return $a; };
+
+$Petal::Hash::MODIFIERS->{'gt:'} = sub { my ($hash, $args) = @_;
+	my ($a, $b, $e1, $e2);
+	($e1, $e2) = jmdicttal::split_args ($args);
+	$a = $hash->jmdicttal::fetch_arg ($e1);
+	$b = $hash->jmdicttal::fetch_arg ($e2);
+	return $a > $b; };
+
+$Petal::Hash::MODIFIERS->{'lt:'} = sub { my ($hash, $args) = @_;
+	my ($a, $b, $e1, $e2);
+	($e1, $e2) = jmdicttal::split_args ($args);
+	$a = $hash->jmdicttal::fetch_arg ($e1);
+	$b = $hash->jmdicttal::fetch_arg ($e2);
+	return $a < $b; };
+
+$Petal::Hash::MODIFIERS->{'len:'} = sub { my ($hash, $args) = @_;
+	my $a = $hash->jmdicttal::fetch_arg ($args);
+	return scalar (@$a); };
+
+$Petal::Hash::MODIFIERS->{'kwabbr:'} = sub { my ($hash, $args) = @_;
+	my (@a, $t, $e, $r);
+	@a = jmdicttal::split_args ($args);
+	$t = $hash->jmdicttal::fetch_arg ($a[0]);
+	$e = $hash->jmdicttal::fetch_arg ($a[1]);
+	return $::KW->{$t}{$e}{kw}; };
 
 $Petal::Hash::MODIFIERS->{'kwabbrs:'} = sub { my ($hash, $args) = @_;
-	my ($typ, $delim, $expr, $x, @a);
-	($typ, $delim, $expr) = split (/\s+'(.*?)'\s+/, $args);
-	$x = $hash->fetch ($expr);
-	@a = map ($::KW->{$typ}{$_->{kw}}{kw}, @$x);
-	$delim =~ s/[\\]([^\\])/$1/go;
-	return join ($delim, @a); };
+	my ( @a, $t, $d, $e);
+	@a = jmdicttal::split_args ($args);
+	$t = $hash->jmdicttal::fetch_arg ($a[0]);
+	$d = $hash->jmdicttal::fetch_arg ($a[1]);
+	$e = $hash->jmdicttal::fetch_arg ($a[2]);
+	@a = map ($::KW->{$t}{$_->{kw}}{kw}, @$e);
+	return join ($d, @a); };
 
 $Petal::Hash::MODIFIERS->{'kwfull:'} = sub { my ($hash, $args) = @_;
-	my ($typ, $expr, $x, $a);
-	($typ, $expr) = split (/ /, $args);
-	$x = $hash->fetch ($expr);
-	$a = $::KW->{$typ}{$x}{descr};
-	return $a; };
+	my (@a, $t, $e, $a);
+	@a = jmdicttal::split_args ($args);
+	$t = $hash->jmdicttal::fetch_arg  ($a[0]);
+	$e = $hash->jmdicttal::fetch_arg  ($a[1]);
+	return $::KW->{$t}{$e}{descr}; };
 
 $Petal::Hash::MODIFIERS->{'kwfulls:'} = sub { my ($hash, $args) = @_;
-	my ($typ, $delim, $expr, $x, @a);
-	($typ, $delim, $expr) = split (/\s+'(.*?)'\s+/, $args);
-	$x = $hash->fetch ($expr);
-	@a = map ($::KW->{$typ}{$_->{kw}}{descr}, @$x);
-	$delim =~ s/[\\]([^\\])/$\1/go;
-	return join ($delim, @a); };
+	my (@a, $t, $d, $e);
+	@a = jmdicttal::split_args ($args);
+	$t = $hash->jmdicttal::fetch_arg ($a[0]);
+	$d = $hash->jmdicttal::fetch_arg ($a[1]);
+	$e = $hash->jmdicttal::fetch_arg ($a[2]);
+	@a = map ($::KW->{$t}{$_->{kw}}{descr}, @$e);
+	return join ($d, @a); };
 
 $Petal::Hash::MODIFIERS->{'freqs:'} = sub { my ($hash, $args) = @_;
-	my ($typ, $delim, $expr, $x, @a);
-	($typ, $delim, $expr) = split (/\s+'(.*?)'\s+/, $args);
-	$x = $hash->fetch ($expr);
-	@a = map ($::KW->{$typ}{$_->{kw}}{kw}.($_->{value}), @$x);
-	$delim =~ s/[\\]([^\\])/$1/go;
-	return join ($delim, @a); };
+	my ( @a, $d, $e);
+	@a = jmdicttal::split_args ($args);
+	$d = $hash->jmdicttal::fetch_arg ($a[0]);
+	$e = $hash->jmdicttal::fetch_arg ($a[1]);
+	@a = map ($::KW->{FREQ}{$_->{kw}}{kw}.($_->{value}), @$e);
+	return join ($d, @a); };
 
 1;

4d21fbe22f57039dee95893aab9c747570c24a84
stuart <none@none>
2007-01-15 12:42:44 -0700

    Add experimental functions dupentr and delentr.

--- pg/mkviews.sql
+++ pg/mkviews.sql
@@ -102,3 +102,113 @@ CREATE VIEW sref AS (
   FROM sens s  
     JOIN entr e ON e.id=s.entr);
 
+-------------------------------------------------------------
+-- This function will replicate an entry by duplicating it's
+-- row in table entr and all child rows recursively (although
+-- this function is not recursive.)
+-------------------------------------------------------------
+CREATE OR REPLACE FUNCTION dupentr(entrid int) RETURNS INT AS $$
+    DECLARE
+	_p0_ INT; _p1_ INT; rec RECORD; REC2 record; rc INT;
+    BEGIN
+	INSERT INTO entr(src,seq,stat,notes) 
+	  (SELECT src,seq,3,notes FROM entr WHERE id=entrid);
+	SELECT lastval() INTO _p0_;
+
+	INSERT INTO dial(entr,kw) 
+	  (SELECT _p0_,kw FROM dial WHERE dial.entr=entrid);
+	INSERT INTO lang(entr,kw) 
+	  (SELECT _p0_,kw FROM lang WHERE lang.entr=entrid);
+	INSERT INTO hist(entr,stat,dt,who,diff,notes) 
+	  (SELECT _p0_,stat,dt,who,diff,notes FROM hist WHERE hist.entr=entrid);
+
+	FOR rec IN (SELECT * FROM kanj WHERE entr=entrid) LOOP
+	    INSERT INTO kanj(entr,ord,txt) VALUES(_p0_,rec.ord,rec.txt);
+	    SELECT lastval() INTO _p1_;
+	    INSERT INTO kinf(kanj,kw) 
+	      (SELECT _p1_,kw FROM kinf WHERE kinf.kanj=rec.id);
+	    INSERT INTO kfreq(kanj,kw,value) 
+	      (SELECT _p1_,kw,value FROM kfreq WHERE kfreq.kanj=rec.id);
+	    END LOOP;
+	FOR rec IN (SELECT * FROM rdng WHERE entr=entrid) LOOP
+	    INSERT INTO rdng(entr,ord,txt) VALUES(_p0_,rec.ord,rec.txt);
+	    SELECT lastval() INTO _p1_;
+	    INSERT INTO rinf(rdng,kw) 
+	      (SELECT _p1_,kw FROM rinf WHERE rinf.rdng=rec.id);
+	    INSERT INTO rfreq(rdng,kw,value) 
+	      (SELECT _p1_,kw,value FROM rfreq WHERE rfreq.rdng=rec.id);
+	    INSERT INTO audio(rdng,fname,strt,leng) 
+	      (SELECT _p1_,fname,strt,leng FROM audio WHERE audio.rdng=rec.id);
+	    END LOOP;
+	FOR rec IN (SELECT * FROM sens WHERE entr=entrid) LOOP
+	    INSERT INTO sens(entr,ord,notes) VALUES(_p0_,rec.ord,rec.notes);
+	    SELECT lastval() INTO _p1_;
+
+	    INSERT INTO pos(sens,kw) 
+	      (SELECT _p1_,kw FROM pos WHERE pos.sens=rec.id);
+	    INSERT INTO misc(sens,kw) 
+	      (SELECT _p1_,kw FROM misc WHERE misc.sens=rec.id);
+	    INSERT INTO fld(sens,kw) 
+	      (SELECT _p1_,kw FROM fld WHERE fld.sens=rec.id);
+	    INSERT INTO gloss(sens,ord,lang,txt,notes) 
+	      (SELECT _p1_,ord,lang,txt,notes FROM gloss WHERE sens=rec.id);
+	    INSERT INTO xref(sens,xref,typ,notes) 
+	      (SELECT _p1_,xref,typ,notes FROM xref WHERE xref.sens=rec.id);
+	    INSERT INTO xref(sens,xref,typ,notes) 
+	      (SELECT sens,_p1_,typ,notes FROM xref WHERE xref.xref=rec.id);
+	    END LOOP;
+
+	-- Duplicate the restrictions.  To match up the old and new rdng
+	-- kanj, and sens rows, we rely on the "ord" column which means the 
+	-- rdng, kanj, and sens ord columns must be unique (have a unique 
+	-- index) within an entry.
+
+	INSERT INTO restr(rdng,kanj)
+	  (SELECT rn.id,kn.id FROM restr z 
+	      JOIN rdng ro ON z.rdng=ro.id 
+	      JOIN rdng rn ON rn.ord=ro.ord
+	      JOIN kanj ko ON ko.id=z.kanj
+	      JOIN kanj kn ON kn.ord=ko.ord
+	      WHERE ro.entr=entrid AND rn.entr=_p0_ AND kn.entr=_p0_);
+
+	INSERT INTO stagr(sens,rdng)
+	  (SELECT sn.id,rn.id FROM stagr z 
+	      JOIN sens so ON z.sens=so.id 
+	      JOIN sens sn ON sn.ord=so.ord
+	      JOIN rdng ro ON ro.id=z.rdng
+	      JOIN rdng rn ON rn.ord=ro.ord
+	      WHERE so.entr=entrid AND sn.entr=_p0_ AND rn.entr=_p0_);
+
+	INSERT INTO stagk(sens,kanj)
+	  (SELECT sn.id,kn.id FROM stagk z 
+	      JOIN sens so ON z.sens=so.id 
+	      JOIN sens sn ON sn.ord=so.ord
+	      JOIN kanj ko ON ko.id=z.kanj
+	      JOIN kanj kn ON kn.ord=ko.ord
+	      WHERE so.entr=entrid AND sn.entr=_p0_ AND kn.entr=_p0_);
+
+	RETURN _p0_;
+	END;
+    $$ LANGUAGE plpgsql;
+
+-------------------------------------------------------------
+-- This function will delete an entry by deleting all related
+-- rows in child tables other than hist, and setting the entr
+-- row's status to "deleted, pending approval".  
+-------------------------------------------------------------
+CREATE OR REPLACE FUNCTION delentr(entrid int) RETURNS void AS $$
+    BEGIN
+	-- Because fk's use "on delete cascade" options we 
+	-- need only delete the top-level children to get 
+	-- rid of everything.
+	UPDATE entr SET stat=5 WHERE entr=entrid;
+	DELETE FROM dial WHERE entr=entrid;
+	DELETE FROM lang WHERE entr=entrid;
+	DELETE FROM kanj WHERE entr=entrid;
+	DELETE FROM rdng WHERE entr=entrid;
+	DELETE FROM sens WHERE entr=entrid;
+	UPDATE entr SET stat=5 WHERE entr=entrid;
+	END;
+    $$ LANGUAGE plpgsql;
+
+

2d9ec3ad892dfc69bea15b5381ab9fa4df69e051
stuart <none@none>
2007-01-15 12:43:04 -0700

    Shorten 'descr' text a little.

--- pg/data/kwstat.sql
+++ pg/data/kwstat.sql
@@ -1,8 +1,8 @@
-INSERT INTO kwstat(id,kw,descr) VALUES(1,'N','New entry, approval pending');
-INSERT INTO kwstat(id,kw,descr) VALUES(2,'A','Active entry');
-INSERT INTO kwstat(id,kw,descr) VALUES(3,'M','Modified entry, approval pending');
-INSERT INTO kwstat(id,kw,descr) VALUES(4,'X','Deleted entry');
-INSERT INTO kwstat(id,kw,descr) VALUES(5,'D','Deleted entry, approval pending');
-INSERT INTO kwstat(id,kw,descr) VALUES(6,'O','Obsoleted entry');
-INSERT INTO kwstat(id,kw,descr) VALUES(8,'R','Rejected entry');
+INSERT INTO kwstat(id,kw,descr) VALUES(1,'N','New, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(2,'A','Active');
+INSERT INTO kwstat(id,kw,descr) VALUES(3,'M','Modified, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(4,'X','Deleted');
+INSERT INTO kwstat(id,kw,descr) VALUES(5,'D','Deleted, approval pending');
+INSERT INTO kwstat(id,kw,descr) VALUES(6,'O','Obsoleted');
+INSERT INTO kwstat(id,kw,descr) VALUES(8,'R','Rejected');
 

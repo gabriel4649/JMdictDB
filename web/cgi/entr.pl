868b3c0da272204e364c1a3f2995e3723e4d7cb8
stuart <none@none>
2007-01-27 23:05:26 -0700

    Add validation of url parameters. Generate error page if parameters are bad..

--- web/cgi/entr.pl
+++ web/cgi/entr.pl
@@ -5,7 +5,8 @@
 	    substr('$Date$',7,-11));
 
 use strict; use warnings;
-use Cwd; use CGI; use Encode; use utf8; use DBI; 
+use Cwd; use CGI; use HTML::Entities;
+use Encode; use utf8; use DBI; 
 use Petal; use Petal::Utils; 
 
 BEGIN {push (@INC, "../lib");}
@@ -13,13 +14,19 @@ use jmdict; use jmdicttal;
 
 $|=1;
 binmode (STDOUT, ":utf8");
+*ee = \&encode_entities;
 
     main: {
-	my ($dbh, $cgi, $tmptbl, $tmpl, $sql, $entries, $e, @qlist, 
+	my ($dbh, $cgi, $tmptbl, $tmpl, $sql, $entries, $e, @qlist, @errs,
 	    @elist, @slist, $sens, @whr, $dbname, $username, $pw, $s);
 	binmode (STDOUT, ":encoding(utf-8)");
 	$cgi = new CGI;
 	print "Content-type: text/html\n\n";
+
+	@qlist = $cgi->param ('q'); validateq (\@qlist, \@errs);
+	@elist = $cgi->param ('e'); validaten (\@elist, \@errs);
+	@slist = $cgi->param ('s'); validaten (\@slist, \@errs);
+	if (@errs) { errors_page (\@errs);  exit; } 
 
 	open (F, "../lib/jmdict.cfg") or die ("Can't open database config file\n");
 	($dbname, $username, $pw) = split (' ', <F>); close (F);
@@ -28,9 +35,6 @@ binmode (STDOUT, ":utf8");
 	$dbh->{pg_enable_utf8} = 1;
 	$::KW = Kwds ($dbh);
 
-	@qlist = $cgi->param ('q'); 
-	@elist = $cgi->param ('e'); 
-	@slist = $cgi->param ('s');
 	if (@qlist) { push (@whr, "e.seq IN (" . join(",",map('?',(@qlist))) . ")"); }
 	if (@elist) { push (@whr, "e.id  IN (" . join(",",map('?',(@elist))) . ")"); }
 	if (@slist) { push (@whr, "s.id  IN (" . join(",",map('?',(@slist))) . ")"); }
@@ -63,6 +67,39 @@ binmode (STDOUT, ":utf8");
 		if ($r->{_audio}) { $found = 1; last; } }
 	    $e->{has_audio} = $found; } }
 
+    sub validaten { my ($list, $errs) = @_;
+	foreach my $p (@$list) {
+	    if (!($p =~ m/^\s*\d+\s*$/)) {
+		push (@$errs, "<br>Bad url parameter received: ".ee($p)); } } }
 
+    sub validateq { my ($list, $errs) = @_;
+	foreach my $p (@$list) {
+	    if (!($p =~ m/^\s*\d{7}\s*$/)) {
+		push (@$errs, "<br>Bad url parameter received: ".ee($p)); } } }
 
-
+    sub errors_page { my ($errs) = @_;
+	my $err_details = join ("\n    ", @$errs);
+	print <<EOT;
+<html>
+<head>
+  <title>Invalid parameters</title>
+  </head>
+<body>
+  <h2>URL Parameter errors</h2>
+  $err_details
+  <hr>
+  Parameter format:<br/>
+  <table border="0">
+    <tr><td>q=dddddd</td> 
+      <td>Display the entry having JMdict sequence number &lt;ddddddd&gt; (a 7-digit number).</td></tr>
+    <tr><td>e=n</td> 
+      <td>Display the entry having database id number &lt;n&gt;.</td></tr>
+    <tr><td>s=n</td> 
+      <td>Display the entry that contains the sense having database id number &lt;n&gt;.</td></tr>
+    </table>
+  Parameter types may be freely intermixed and any number of parameters 
+  may be given in a single url.
+  </body>
+</html>
+EOT
+	}

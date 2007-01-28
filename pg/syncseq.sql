5e99482b94ccac0841d4dfbc58a21ee19a216102
stuart <none@none>
2007-01-27 23:12:33 -0700

    Update the new "seq" sequence (see mktables.sql).

--- pg/syncseq.sql
+++ pg/syncseq.sql
@@ -8,4 +8,6 @@ SELECT setval('hist_id_seq', (SELECT max(id) FROM hist));
 SELECT setval('kanj_id_seq', (SELECT max(id) FROM kanj));
 SELECT setval('rdng_id_seq', (SELECT max(id) FROM rdng));
 SELECT setval('sens_id_seq', (SELECT max(id) FROM sens));
+SELECT setval('sens_id_seq', (SELECT max(id) FROM sens));
+SELECT setval('seq', (SELECT MAX(seq) FROM entr WHERE seq<9000000));
 

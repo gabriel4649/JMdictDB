fb78a30c0670a8a10d070c9e6464efcf9b236308
stuart <none@none>
2007-02-11 22:35:48 -0700

    Pk/fk changes on major tables. Changes corresponding to the jmdict modularization changes. Remove id search capability for sens, rdng, kanj, gloss. build_search_sql() and extend_from() (nee mk_from_clause())   were complete rewritten.

--- web/cgi/srchres.pl
+++ web/cgi/srchres.pl
@@ -6,7 +6,7 @@
 
 use strict; use warnings;
 use Cwd; use CGI; use Encode 'decode_utf8'; use DBI; 
-use Petal; use Petal::Utils; 
+use Petal; use Petal::Utils; use Time::HiRes('time');
 
 BEGIN {push (@INC, "../lib");}
 use jmdict; use jmdicttal;
@@ -39,25 +39,28 @@ binmode (STDOUT, ":utf8");
 
 	if ($idval) {	# Search for id number...
 	    if ($idtbl ne "seqnum") { $col = "id"; }
-	    else { $idtbl = "entr";  $col = "seq"; }
+	    else { $idtbl = "entr e";  $col = "seq"; }
 	    ($sql, $sql_args) = build_search_sql (
-		[[$idtbl, sprintf ("%s.%s=?", $idtbl, $col), [$idval]]]); }
+		[[$idtbl, sprintf ("e.%s=?", $col), [$idval]]]); }
 	else {
 	    for $i (0..2) {
 		if ($t[$i]) { 
 		    push (@condlist, str_match_clause ($s[$i],$y[$i],$t[$i],$i)); } }
-	    if (@pos)  { push (@condlist, ["pos", getsel("pos.kw",   \@pos), []]); }
-	    if (@misc) { push (@condlist, ["misc",getsel("misc.kw",  \@misc),[]]); }
-	    if (@src)  { push (@condlist, ["entr",getsel("entr.src", \@src), []]); }
-	    if (@stat) { push (@condlist, ["entr",getsel("entr.stat",\@stat),[]]); }
+	    if (@pos)  { push (@condlist, ["pos", getsel("pos.kw", \@pos), []]); }
+	    if (@misc) { push (@condlist, ["misc",getsel("misc.kw", \@misc),[]]); }
+	    if (@src)  { push (@condlist, ["entr e",getsel("e.src", \@src), []]); }
+	    if (@stat) { push (@condlist, ["entr e",getsel("e.stat", \@stat),[]]); }
 	    if (@freq) { push (@condlist, freq_srch_clause (\@freq, $nfval, $nfcmp, $gaval, $gacmp)); }
 	    ($sql, $sql_args) = build_search_sql (\@condlist); }
 
-	$sql2 = sprintf ("SELECT q.* FROM entr_summary q JOIN (%s) AS i ON i.id=q.id", $sql);
+	$::Debug->{'Search sql'} = $sql;  $::Debug->{'Search args'} = join(",", @$sql_args);
+	$sql2 = sprintf ("SELECT q.* FROM esum q JOIN (%s) AS i ON i.id=q.id", $sql);
+	my $start = time();
 	eval { $rs = dbread ($dbh, $sql2, $sql_args); };
+	$::Debug->{'Search time'} = time() - $start;
 	if ($@) {
 	    print "Content-type: text/html\n\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body>";
-	    print "<pre> $@ </pre>\n<pre>$sql2<\pre>\n<pre>".join(", ", @$sql_args)."</pre></body></html>\n";
+	    print "<pre> $@ </pre>\n<pre>$sql2</pre>\n<pre>".join(", ", @$sql_args)."</pre></body></html>\n";
 	    exit (1);
 	    }
 	if (scalar (@$rs) == 1) {
@@ -66,15 +69,15 @@ binmode (STDOUT, ":utf8");
 	    print "Content-type: text/html\n\n";
 	    $tmpl = new Petal (file=>'../lib/tal/srchres.tal', 
 			   decode_charset=>'utf-8', output=>'HTML' );
-	    print $tmpl->process (results=>$rs, sql=>$sql); }
+	    print $tmpl->process (results=>$rs, dbg=>$::Debug); }
 	$dbh->disconnect; }
 
     sub str_match_clause { my ($srchin, $srchtyp, $srchtxt, $idx) = @_; 
 	my ($x, $table, $alias, $whr, @args);
 	if ($srchin eq "auto") {
 	    $x = jstr_classify ($srchtxt);
-	    if ($x & $::KANJI) { $table = "kanj";  }
-	    elsif ($x & $::KANA) { $table = "rdng"; }
+	    if ($x & $jmdict::KANJI) { $table = "kanj";  }
+	    elsif ($x & $jmdict::KANA) { $table = "rdng"; }
 	    else { $table = "gloss"; } }
 	else { $table = $srchin; }
 	$alias = " " . {kanj=>"j", rdng=>"r", gloss=>"g"}->{$table} . "$idx";
@@ -141,8 +144,11 @@ binmode (STDOUT, ":utf8");
 	#     on.  The name may optionally be followed by a space and
 	#     an alias name for the table.  It may also optionally be
 	#     preceeded (no space) by an astrisk character to indicate
-	#     the table should be joinded with a LEFT JOIN rather than
+	#     the table should be joined with a LEFT JOIN rather than
 	#     the default INNER JOIN. 
+	#     Caution: if the table is "entr" it *must* have "e" as an
+	#     alias, since that alias is expected by the final generated
+	#     sql.
 	#   1: Sql snippit that will be AND'd into the WHERE clause.
 	#     Field names must be qualified by table.  When looking 
 	#     for a value in a field.  A "?" may (and should) be used 
@@ -153,69 +159,160 @@ binmode (STDOUT, ":utf8");
 	#     ("?") used in the second value of the tuple (see #1 above).
 	#
 	# Example:
-	#     [("entr","entr.typ=1", ()),
+	#     [("entr e","e.typ=1", ()),
 	#      ("gloss", "gloss.text LIKE ?", ("'%'+but+'%'",)),
 	#      ("pos","pos.kw IN (?,?,?)",(8,18,47))]
 	#
 	#   This will generate the SQL statement and arguments:
-	#     "SELECT entr.id FROM (((entr INNER JOIN sens ON sens.entr=entr.id) 
+	#     "SELECT e.id FROM (((entr e INNER JOIN sens ON sens.entr=entr.id) 
 	# 	INNER JOIN gloss ON gloss.sens=sens.id) 
 	# 	INNER JOIN pos ON pos.sens=sens.id) 
-	# 	WHERE entr.typ=1 AND (gloss.text=?) AND (pos IN (?,?,?))"
+	# 	WHERE e.typ=1 AND (gloss.text=?) AND (pos IN (?,?,?))"
 	#     ('but',8,18,47)
 	#   which will find all entries that have a gloss containing the
 	#   substring "but" and a sense with a pos (part-of-speech) tagged
 	#   as a conjunction (pos.kw=8), a particle (18), or an irregular
 	#   verb (47).
+	# 
+	#   
 
-	my (%rels, %tables, @tables, @wclauses, @args, $tbl, 
-	    $cond, $arg, $tnm, $frm, $where, $sql, $itm);
+	my ($fclause, $cnum,  @wclauses, @args, $jt, $alias,
+	    $cond, $arg, $tbl, $where, $sql, $itm, $jointype);
 
-	%rels = (dial=>"entr", lang=>"entr", hist=>"entr",
-		 rdng=>"entr", kanj=>"entr", sens=>"entr",
-		 rinf=>"rdng", rfreq=>"rdng", audio=>"rdng", stagr=>"rdng",
-		 kinf=>"kanj", kfreq=>"kanj",
-		 gloss=>"sens", pos=>"sens", misc=>"sens", fld=>"sens", 
-		 stagr=>"sens", stagk=>"sens", xref=>"sens", );
+	# $fclause will become the FROM clause of the generated sql.  Since
+	# all queries will rquire "entr" to be included, we start of with 
+	# that table in the clause.
+
+	$fclause = "entr e";  $cnum = 0;
+
+	# Go through the condition list.  For each 3-tuple we will add the
+	# table name to the FROM clause, and the where and arguments items 
+	# to there own arrays.
 
 	foreach $itm (@$condlist) {
+
+	    # extend_from() requires a number that it uses to generate 
+	    # unique table aliases.  We will use $cnum for this.
+
+	    $cnum += 1;
+
+	    # unpack a @$condist 3-tuple.
+
 	    ($tbl, $cond, $arg) = @$itm;
-	      ##Add tbl, and all of tbl's parents to the table list.  
-	    while ($tbl) {
-		$tnm = ($tbl =~ m/([_a-zA-Z0-9]+)/)[0];
-		$tables{$tbl} = 1;
-		$tbl = $rels{$tnm}; }
+
+	    # The table name may be preceeded by a "*" to indicate that
+	    # it is to be joinged with a LEFT JOIN rather than the usual
+	    # INNER JOIN".  It may also be followed by a space and an 
+	    # alias name.  Unpack these things.
+ 
+	    ($jt, $tbl, undef, $alias) = ($tbl =~ m/^([*])?(\w+)(\s+(\w+))?$/);
+	    $jointype = $jt ? "LEFT JOIN" : "JOIN";
+
+	    # Add the table (using the desired alias if any) to the FROM 
+	    # clause in $fclause.  extend_from() will take care of joining
+	    # any needed parent tables.
+	
+	    $fclause = extend_from ($fclause, $tbl, $alias, $jointype, $cnum, 1);
+
+	    # extend_clause() always returns a string with parens around 
+	    # the "entr" table which is invalid sql.  Remove the parens.
+ 
+	    $fclause =~ s/\(entr e\)/entr e/;
+
+	    # Save the cond tuple's where clause and arguments each in 
+	    # their own array.
+
 	    push (@wclauses, $cond);
 	    push (@args, @$arg); }
-	$frm = mk_from_clause( [keys (%tables)], \%rels );
+
+	# AND all the where clauses together.
+
 	$where = join (" AND ", grep ($_, @wclauses));
-	$sql = sprintf ("SELECT DISTINCT entr.id FROM %s WHERE %s", $frm, $where);
+
+	# Create the sql we need to find the entr.id numbers from 
+	# the tables and where conditions given in the @$condlist.
+
+	$sql = sprintf ("SELECT DISTINCT e.id FROM %s WHERE %s", $fclause, $where);
+
+	# Return the sql and the arguments which are now suitable
+	# for execution.
+
 	return ($sql, \@args); }
 
-    sub mk_from_clause { my ($tables, $rels) = @_;
-	# Given a list of tables, @$tables, and a hash, %$rels, that 
-	# for each possible table in 'tables' give the parent table, 
-	# create a string that can be used in a SQL "FROM" clause that
-	# joins all the tables.  We assume that the primary key column
-	# of each parent table is named "id" and the foreign key column
-	# of each child table has the same name as the parent table. 
-	# The table in 'tables' may be just a table name, or may be a
-	# table name followed by a space and an alias.
+    sub extend_from { my ($clause, $table, $alias, $jointype, $branch, $depth) = @_;
 
-	my (@otables, $clause, $tb, $tx, $tbl, $join, $alias);
-	@otables = ("entr","rdng","rinf","rfreq","kanj","kinf","kfreq",
-		    "sens","gloss","pos","misc","fld","dial","lang");
-	$clause = "";
-	foreach $tb (@otables) {
-	    foreach $tx (@$tables) {
-		($tbl, $alias) = (split (' ', $tx), "");
-		if (substr ($tbl, 0, 1) eq "*") {
-		    $join = "LEFT JOIN";
-		    $tbl = substr ($tbl, 1); }
-		else { $join = "JOIN"; }
-		next if ($tbl ne $tb);
-		if (!$clause) { $clause = $tbl; }
-		else { $clause = sprintf ("(%s %s %s %s ON %s.%s=%s.%s)" , 
-		    $clause, $join, $tbl, $alias, $alias || $tbl, 
-		    $rels->{$tbl}, $rels->{$tbl}, "id"); } } }
-	return $clause; }
+	# Extend an existing FROM clause, $clause, by joining $table
+	# to it.  $alias is an alias for table and may be "" or undef
+	# in which case no alias will be used.  $jointype is either 
+	# "JOIN" or "LEFT JOIN" and controls how $table will be joined
+	# to $clause.  $branch is an integer used to generate unique
+	# aliases table aliases and should be a different value for 
+	# each (non-recursive) call.  $depth should be 1 and is only
+	# used for checking for accidental non-terminating recursion.
+
+	my ($t, $p, $parent, $joinpairs, $oncls, $palias, $Tables, $aliasdot);
+
+	# $jmdict::Tables describes the relations between the jmdict tables.
+
+	$Tables = $jmdict::Tables;
+
+	# Sanity checks...
+
+	if ($depth > 9) { die "extend_from(): recursion too deep\n"; }
+	if (!($t = $Tables->{$table})) {	# That's "=", not "==".
+	    die ("extend_from(): Unknown table: $table\n")}; 
+
+	$alias = $alias || "";  $aliasdot = ($alias || $table) . ".";  
+	$parent = $t->{parent};		# Name of $tables' parent table.
+
+	# If there is no parent, then this table in "entr" and $clause
+	# already includes it, so we need not make any changes.
+
+	if (!$parent) { return $clause; }
+
+	# Our $table is not "entr" so we will need to join it to it's
+	# parent table.  Get a ref to the parent info ($p) and the 
+	# alias to use ($palias)  $branch is used to make sure that 
+	# the parent alias is unique since its table may occur more
+	# that once in the final query.
+
+	$p = $Tables->{$parent};
+	$palias = $p->{al} . ($parent eq "entr" ? "" : "$branch");
+
+	# The parent table and our table may be joined on a composite 
+	# (i.e. multiple) key, so we pair up each element in the 
+	# parent's primary key with the corresponding elemnent from 
+	# $table's foreign key.  Each table column name needs to be 
+	# qualified by the table alias, which is done by the two map() 
+	# calls.  The zip() functiob (from module jmdict) does the 
+	# pairing.  The result $joinpairs is a ref to an array of 
+	# 2-tuples, where each 2-tuple is a properly qualified (fk,pk) 
+	# pair.
+
+	$joinpairs = zip ([map("$aliasdot$_", @{$t->{fk}})], 
+			  [map("$palias.$_", @{$p->{pk}})]);
+
+	# Take each pair and join into a string with "=" and then join
+	# all of those with "AND" to get a string we can use for the
+	# join's ON clause.
+
+	$oncls = join (" AND ", map (join ("=", @$_), @$joinpairs));
+
+	if ($parent ne "entr") {
+
+	    # When called the first time by build_search_sql(), $clause 
+	    # will contain the table "entr" since this table is always 
+	    # required.  If $table's parent is "entr" than it can be 
+	    # joined directly to the "entr" in $clause.  But otherwise, 
+	    # it's parent table need to be in $clause before we can join
+	    # it to $clause.  We arrange this by calling ourself recursively
+	    # for our parent table, which causes all the needed parent
+	    # tables to be in $clause.
+
+	    $clause = extend_from ($clause, $parent, $palias, "JOIN", $branch, $depth+1); }
+
+	# Here we know all needed parent tables have already been joined
+	# so all be need to do is join our $table.
+
+	return "($clause) $jointype $table $alias ON $oncls"; }
+

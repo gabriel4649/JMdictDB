f668b625845b976332b54b003d2b2db874f1adb5
stuart <none@none>
2007-02-11 22:32:50 -0700

    Pk/fk changes on major tables. Use "Deleted..." comments to create D history records.   Unparsable comments are written to new output file   skipped_comments.log. Add -e option to specify encoding used for interactive   and debug messages.  Program still bombs when converting   unconvertable characters (european character in gloss to   shift_jis on Windows for example) which limits usefulness   of this option.

--- perl/jmparse.pl
+++ perl/jmparse.pl
@@ -6,7 +6,6 @@
 # appropriately configured Postgresql database.
 #
 # Special handling: 
-# - generate "ord" values.
 # - propagate PoS through senses.
 # - do xrefs/ants in 2nd pass
 # - ke_pri, re_pri: ignore (redundant) newsX values.
@@ -20,7 +19,6 @@
 # Add command line option to give a directory for the
 #   temporary files.
 # Better performance if we use SAX instead of XML::Twig?
-# Derive encoding used for interactive output from locale.
 # shift_jis output encoding on windows doesn't really
 #   work -- get unmappable utf8 characters in gloss which
 #   cause fatal error.  Should non-fatally map to a printable
@@ -34,37 +32,36 @@ use strict;
 BEGIN {push (@INC, "./lib");}
 use jmdictxml ('%JM2ID');
 
-if (lc ($ENV{OS}) =~ m/windows/) {
-    binmode(STDOUT, ":encoding(shift_jis)");
-    binmode(STDERR, ":encoding(shift_jis)");
-    eval { binmode($DB::OUT, ":encoding(utf-8)"); } }
-else {
-    binmode(STDOUT, ":encoding(utf-8)");
-    binmode(STDERR, ":encoding(utf-8)");
-    eval { binmode($DB::OUT, ":encoding(utf-8)"); } }
-
-$::Ordincr = 1;	# "ord" columns in tables rdng, kanj, sens and gloss 
-		#   will start at and be incremented by this value.
-
 main: {
-	my ($twig, $infn, $outfn, $tmpfiles, $tmp);
+	my ($twig, $infn, $outfn, $tmpfiles, $tmp, $enc);
 
-	getopts ("o:c:s:kh", \%::Opts);
+	getopts ("o:c:s:e:kh", \%::Opts);
 	if ($::Opts{h}) { usage (0); }
+	$enc = $::Opts{e} || "utf-8";
+	binmode(STDOUT, ":encoding($enc)");
+	binmode(STDERR, ":encoding($enc)");
+	eval { binmode($DB::OUT, ":encoding($enc)"); };
+
 	$infn = shift (@ARGV) || "JMdict";
 	$outfn = $::Opts{o} || "JMdict.dmp";
 
 	  # Make STDERR unbuffered so we print "."s, one at 
 	  # a time, as a sort of progress bar.  
 	$tmp = select(STDERR); $| = 1; select($tmp);
+
 	  # Create an XML parser instance.  'twig_roots' is
 	  # a hash whose keys (one) name the elements to be 
 	  # parsed, and value(s) give the name of a subroutine
 	  # to be called to process the parsed element. 
-	$twig = XML::Twig->new (twig_roots => { entry => \&entry_ele });
+	$twig = XML::Twig->new (twig_handlers => { 
+				    entry=>\&entry_handler,
+				   '#COMMENT'=>\&comment_handler}, 
+				comments=>'process');
+
 	  # Initialize global variables and open all the 
 	  # temporary table files. 
 	$tmpfiles = initialize ();
+
 	  # Parse the give xml file.  The entry_ele sub given 
 	  # when the $twig was created does all the work of
 	  # writing the parsed data to the tables files.
@@ -72,6 +69,7 @@ main: {
 	eval { $twig->parsefile( $infn ); };
 	die if ($@ and $@ ne "done\n"); 
 	print STDERR "\n";
+
 	  # Now merge all the temp table files into one big 
 	  # dump file that can be fed to Postgresql.
 	print STDERR "Generating output file $outfn\n";
@@ -82,34 +80,35 @@ sub initialize {
 	my ($t);
 	my @tmpfiles = (
 	  [\$::Fentr, "load01.tmp", "COPY entr(id,src,seq,stat,notes) FROM stdin;"],
-	  [\$::Fkanj, "load02.tmp", "COPY kanj(id,entr,ord,txt) FROM stdin;"],
-	  [\$::Fkinf, "load03.tmp", "COPY kinf(kanj,kw) FROM stdin;"],
-	  [\$::Fkfrq, "load04.tmp", "COPY kfreq(kanj,kw,value) FROM stdin;"],
-	  [\$::Frdng, "load05.tmp", "COPY rdng(id,entr,ord,txt) FROM stdin;"],
-	  [\$::Frinf, "load06.tmp", "COPY rinf(rdng,kw) FROM stdin;"],
-	  [\$::Frfrq, "load07.tmp", "COPY rfreq(rdng,kw,value) FROM stdin;"],
-	  [\$::Fsens, "load08.tmp", "COPY sens(id,entr,ord,notes) FROM stdin;"],
-	  [\$::Fpos,  "load09.tmp", "COPY pos(sens,kw) FROM stdin;"],
-	  [\$::Fmisc, "load10.tmp", "COPY misc(sens,kw) FROM stdin;"],
-	  [\$::Ffld,  "load11.tmp", "COPY fld(sens,kw) FROM stdin;"],
-	  [\$::Fxref, "load12.tmp", "COPY xresolv(sens,typ,txt) FROM stdin;"],
-	  [\$::Fglos, "load13.tmp", "COPY gloss(id,sens,ord,lang,txt,notes) FROM stdin;"],
+	  [\$::Fkanj, "load02.tmp", "COPY kanj(entr,kanj,txt) FROM stdin;"],
+	  [\$::Fkinf, "load03.tmp", "COPY kinf(entr,kanj,kw) FROM stdin;"],
+	  [\$::Fkfrq, "load04.tmp", "COPY kfreq(entr,kanj,kw,value) FROM stdin;"],
+	  [\$::Frdng, "load05.tmp", "COPY rdng(entr,rdng,txt) FROM stdin;"],
+	  [\$::Frinf, "load06.tmp", "COPY rinf(entr,rdng,kw) FROM stdin;"],
+	  [\$::Frfrq, "load07.tmp", "COPY rfreq(entr,rdng,kw,value) FROM stdin;"],
+	  [\$::Fsens, "load08.tmp", "COPY sens(entr,sens,notes) FROM stdin;"],
+	  [\$::Fpos,  "load09.tmp", "COPY pos(entr,sens,kw) FROM stdin;"],
+	  [\$::Fmisc, "load10.tmp", "COPY misc(entr,sens,kw) FROM stdin;"],
+	  [\$::Ffld,  "load11.tmp", "COPY fld(entr,sens,kw) FROM stdin;"],
+	  [\$::Fxref, "load12.tmp", "COPY xresolv(entr,sens,typ,txt) FROM stdin;"],
+	  [\$::Fglos, "load13.tmp", "COPY gloss(entr,sens,gloss,lang,txt,notes) FROM stdin;"],
 	  [\$::Fdial, "load14.tmp", "COPY dial(entr,kw) FROM stdin;"],
 	  [\$::Flang, "load15.tmp", "COPY lang(entr,kw) FROM stdin;"],
-	  [\$::Frestr, "load16.tmp", "COPY restr(rdng,kanj) FROM stdin;"],
-	  [\$::Fstagr, "load17.tmp", "COPY stagr(sens,rdng) FROM stdin;"],
-	  [\$::Fstagk, "load18.tmp", "COPY stagk(sens,kanj) FROM stdin;"],
+	  [\$::Frestr, "load16.tmp", "COPY restr(entr,rdng,kanj) FROM stdin;"],
+	  [\$::Fstagr, "load17.tmp", "COPY stagr(entr,sens,rdng) FROM stdin;"],
+	  [\$::Fstagk, "load18.tmp", "COPY stagk(entr,sens,kanj) FROM stdin;"],
 	  [\$::Fhist, "load19.tmp", "COPY hist(id,entr,stat,dt,who,diff,notes) FROM stdin;"] );
 
 	$::srcid = 1; $::cntr = 0;
 	  # Following globals are used to maintain the row 'id'
-	  # numbers for tables entr, kanj, rdng, sens, gloss,
-	  # and hist respectively.
-	$::eid = $::jid = $::rid = $::sid = $::gid = $::hid = 1;
+	  # numbers for tables entr and hist respectively.
+	$::eid = $::hid = 1;
 
 	foreach $t (@tmpfiles) {
 	    open (${$t->[0]}, ">:utf8", $t->[1]) or \
 		  die ("Can't open $t->[1]: $!\n") }
+	open ($::Fskiplog, ">:utf8", "skipped_comments.log") or \
+	    die ("Can't open load_jmdict_skipped.log: $!\n");
 	return \@tmpfiles; }
 
 sub finalize { my ($outfn, $tmpfls, $del) = @_;
@@ -127,9 +126,9 @@ sub finalize { my ($outfn, $tmpfls, $del) = @_;
 	    if ($del) {unlink ($t->[1]); } } 
 	close (FOUT); } 
 
-sub entry_ele { my( $t, $entry ) = @_;
+sub entry_handler { my ($t, $entry ) = @_;
 	my ($seq, @x, $kmap, $rmap);
-	if (!($::cntr % 1375)) { print STDERR "."; } 
+	if (!($::cntr % 1385)) { print STDERR "."; } 
 	$::cntr += 1;
 	$seq = ($entry->get_xpath("ent_seq"))[0]->text;
 	if (!$::started_at and (!$::Opts{s} or $seq eq $::Opts{s})) { 
@@ -139,6 +138,30 @@ sub entry_ele { my( $t, $entry ) = @_;
 	     die ("done\n"); }
 	do_entry ($seq, $entry);
 	$t->purge; 0;}
+
+sub comment_handler { my ($t, $entry ) = @_;
+	my ($c, $seq, $notes, $dt, $ln); 
+	return if (! $::processing);
+	$c = $entry->{comment};
+	if ($c =~ m/^\s*Deleted:\s*(\d{7}) with (\d{7})\s*(.*)/) {
+	    $seq = $1; $notes = "Merged into $2";
+	    if ($3) { $notes .= "\n" . $3; } }
+	elsif ($c =~ m/^\s*Deleted:\s*(\d{7})\s*(.*)/) {
+	    $seq = $1; if ($2) { $notes = $2; } }
+	else { 
+	    $ln = $t->current_line();
+	    print $::Fskiplog "$ln: $c\n"; return; }
+
+	$dt = "1990-01-01 00:00:00-00";
+	# (id,src,seq,stat,note)
+	print $::Fentr "$::eid\t$::srcid\t$seq\t4\t\\N\n";
+
+	# Should we create a synthetic N record before creating the D record?
+	# (id,entr,stat,dt,who,diff,notes)
+	$notes = pgesc ($notes);
+	print $::Fhist "$::hid\t$::eid\t4\t$dt\tfrom JMdict.xml\t\\N\t$notes\n"; 
+	$::eid += 1;  $::hid += 1; 
+	$t->purge; 0; }
 
 sub do_entry { my ($seq, $entry) = @_;
 	my (@x, $kmap, $rmap);
@@ -154,130 +177,126 @@ sub do_entry { my ($seq, $entry) = @_;
 
 sub do_kanj { my ($keles) = @_;
 	my ($ord, $txt, $k, @x, $kmap);
-	$ord = $::Ordincr; $kmap = {};
+	$ord = 1; $kmap = {};
 	foreach $k (@$keles) {
 	    $txt = ($k->get_xpath ("keb"))[0]->text;
-	    # (id,entr,ord,txt)
-	    print $::Fkanj "$::jid\t$::eid\t$ord\t$txt\n";
-	    if (@x = $k->get_xpath ("ke_inf")) { do_kinfs (\@x); }
-	    if (@x = $k->get_xpath ("ke_pri")) { do_kfrqs (\@x); }
-	    $kmap->{$txt} = $::jid;
-	    $ord += $::Ordincr; 
-	    $::jid += 1; }
+	    # (entr,ord,txt)
+	    print $::Fkanj "$::eid\t$ord\t$txt\n";
+	    if (@x = $k->get_xpath ("ke_inf")) { do_kinfs (\@x, $ord); }
+	    if (@x = $k->get_xpath ("ke_pri")) { do_kfrqs (\@x, $ord); }
+	    $kmap->{$txt} = $ord;
+	    $ord += 1; }
 	return $kmap; }
 
-sub do_kinfs { my ($kinfs) = @_;
+sub do_kinfs { my ($kinfs, $ord) = @_;
 	my ($i, $kw, $txt);
 	foreach $i (@$kinfs) {
 	    $txt = $i->text;
 	    ($kw = $::JM2ID{KINF}{$txt}) or \
 		die ("Unknown ke_inf text: /$txt/\n");
-	    print $::Fkinf "$::jid\t$kw\n"; } }
+	    print $::Fkinf "$::eid\t$ord\t$kw\n"; } }
 
-sub do_kfrqs { my ($kfrqs) = @_;
+sub do_kfrqs { my ($kfrqs, $ord) = @_;
 	my ($kw, $f);
 	$f = freqs ($kfrqs);
 	foreach $kw (sort (keys (%$f))) {
-	    if ($kw) { print $::Fkfrq "$::jid\t$kw\t$f->{$kw}\n"; } } }
+	    if ($kw) { print $::Fkfrq "$::eid\t$ord\t$kw\t$f->{$kw}\n"; } } }
 
 sub do_rdng { my ($reles, $kmap) = @_;
 	my ($ord, $txt, $r, $z, @x, $rmap, %restr);
-	$ord = $::Ordincr;  $rmap = {}; %restr = ();
+	$ord = 1;  $rmap = {}; %restr = ();
 	foreach $r (@$reles) {
 	    $txt = ($r->get_xpath ("reb"))[0]->text;
-	    # (id,entr,ord,txt)
-	    print $::Frdng "$::rid\t$::eid\t$ord\t$txt\n";
-	    if (@x = $r->get_xpath ("re_inf")) { do_rinfs (\@x); }
-	    if (@x = $r->get_xpath ("re_pri")) { do_rfrqs (\@x); }
+	    # (entr,ord,txt)
+	    print $::Frdng "$::eid\t$ord\t$txt\n";
+	    if (@x = $r->get_xpath ("re_inf")) { do_rinfs (\@x, $ord); }
+	    if (@x = $r->get_xpath ("re_pri")) { do_rfrqs (\@x, $ord); }
 	    for $z ($r->get_xpath ("re_restr")) { 
-		if (! defined ($restr{$::rid})) { $restr{$::rid} = {}; }
-		$restr{$::rid}->{$kmap->{$z->text}} = 1; }
+		if (! defined ($restr{$ord})) { $restr{$ord} = {}; }
+		$restr{$ord}->{$kmap->{$z->text}} = 1; }
 	    if ($r->get_xpath ("re_nokanji")) { 
-		if (! defined ($restr{$::rid})) { $restr{$::rid} = {}; }
-		$restr{$::rid} = 1; }
-	    $rmap->{$txt} = $::rid;
-	    $ord += $::Ordincr; 
-	    $::rid += 1; }
+		if (! defined ($restr{$ord})) { $restr{$ord} = {}; }
+		$restr{$ord} = 1; }
+	    $rmap->{$txt} = $ord;
+	    $ord += 1; }
 	if (%restr) { do_restr ($::Frestr, \%restr, $rmap, $kmap); }
 	return $rmap; }
 
-sub do_rinfs { my ($rinfs) = @_;
+sub do_rinfs { my ($rinfs, $ord) = @_;
 	my ($i, $kw, $txt);
 	foreach $i (@$rinfs) {
 	    $txt = $i->text;
 	    ($kw = $::JM2ID{RINF}{$txt}) or \
 		die ("Unknown re_inf text: /$txt/\n") ;
-	    print $::Frinf "$::rid\t$kw\n"; } }
+	    print $::Frinf "$::eid\t$ord\t$kw\n"; } }
 
-sub do_rfrqs { my ($rfrqs) = @_;
+sub do_rfrqs { my ($rfrqs, $ord) = @_;
 	my ($kw, $f);
 	$f = freqs ($rfrqs);
 	foreach $kw (sort (keys (%$f))) {
-	    if ($kw) { print $::Frfrq "$::rid\t$kw\t$f->{$kw}\n"; } } }
+	    if ($kw) { print $::Frfrq "$::eid\t$ord\t$kw\t$f->{$kw}\n"; } } }
 
 sub do_sens { my ($sens, $kmap, $rmap) = @_;
 	my ($ord, $txt, $s, @x, @p, @pp, $z, %smap, %stagr, %stagk);
-	$ord=$::Ordincr;  @pp=(); %smap=(); %stagr=(); %stagk=();
+	$ord=1;  @pp=(); %smap=(); %stagr=(); %stagk=();
 	foreach $s (@$sens) {
 	    $txt = "\\N";
 	    if (@x = $s->get_xpath ("s_inf")) { $txt = $x[0]->text; }
-	    # (id,entr,ord,note)
-	    print $::Fsens "$::sid\t$::eid\t$ord\t$txt\n";
+	    # (entr,ord,note)
+	    print $::Fsens "$::eid\t$ord\t$txt\n";
 	    @p = $s->get_xpath ("pos");
 	    if (!@p) { @p = @pp; }
-	    if (@p) { do_pos (\@p); @pp = @p; }
-	    if (@x = $s->get_xpath ("misc"))  { do_misc (\@x); }
-	    if (@x = $s->get_xpath ("field")) { do_fld  (\@x); }
-	    if (@x = $s->get_xpath ("gloss")) { do_glos (\@x); }
-	    if (@x = $s->get_xpath ("xref"))  { do_xref ("see", \@x); }
-	    if (@x = $s->get_xpath ("ant"))   { do_xref ("ant", \@x); }
+	    if (@p) { do_pos (\@p, $ord); @pp = @p; }
+	    if (@x = $s->get_xpath ("misc"))  { do_misc (\@x, $ord); }
+	    if (@x = $s->get_xpath ("field")) { do_fld  (\@x, $ord); }
+	    if (@x = $s->get_xpath ("gloss")) { do_glos (\@x, $ord); }
+	    if (@x = $s->get_xpath ("xref"))  { do_xref ("see", \@x, $ord); }
+	    if (@x = $s->get_xpath ("ant"))   { do_xref ("ant", \@x, $ord); }
 	    for $z ($s->get_xpath ("stagr")) { 
-		if (! defined ($stagr{$::sid})) { $stagr{$::sid} = {}; }
-		$stagr{$::sid}->{$rmap->{$z->text}} = 1; }
+		if (! defined ($stagr{$ord})) { $stagr{$ord} = {}; }
+		$stagr{$ord}->{$rmap->{$z->text}} = 1; }
 	    for $z ($s->get_xpath ("stagk")) { 
-		if (! defined ($stagk{$::sid})) { $stagk{$::sid} = {}; }
-		$stagk{$::sid}->{$kmap->{$z->text}} = 1; }
-	    $smap{$s::id} = $::sid;
-	    $ord += $::Ordincr; 
-	    $::sid += 1; }
+		if (! defined ($stagk{$ord})) { $stagk{$ord} = {}; }
+		$stagk{$ord}->{$kmap->{$z->text}} = 1; }
+	    $smap{$ord} = $ord;
+	    $ord += 1; }
 	if (%stagr) { do_restr ($::Fstagr, \%stagr, \%smap, $rmap); }
 	if (%stagk) { do_restr ($::Fstagk, \%stagk, \%smap, $kmap); } }
 
-sub do_glos { my ($gloss) = @_;
-	my ($g, $ord, $lang, $txt);
-	$ord = $::Ordincr;
+sub do_glos { my ($gloss, $ord) = @_;
+	my ($g, $gord, $lang, $txt);
+	$gord = 1;
 	foreach $g (@$gloss) {
 	    $lang = undef; $lang = $g->att("g_lang");
 	    $lang = $lang ? $::JM2ID{LANG}{$lang} : $::JM2ID{LANG}{"en"}; 
 	    ($txt = $g->text) =~ s/\\/\\\\/go;
-	    # (id,sens,ord,lang,txt,notes)
-	    print $::Fglos "$::gid\t$::sid\t$ord\t$lang\t$txt\t\\N\n"; 
-	    $ord += $::Ordincr;
-	    $::gid += 1; } }
+	    # (entr,sens,ord,lang,txt,notes)
+	    print $::Fglos "$::eid\t$ord\t$gord\t$lang\t$txt\t\\N\n"; 
+	    $gord += 1; } }
 
-sub do_pos { my ($pos) = @_;
+sub do_pos { my ($pos, $ord) = @_;
 	my ($i, $kw, $txt);
 	foreach $i (@$pos) {
 	    $txt = $i->text;
 	    ($kw = $::JM2ID{POS}{$txt}) or \
 		die ("Unknown \'pos\' text: /$txt/\n");
-	    print $::Fpos "$::sid\t$kw\n"; } }
+	    print $::Fpos "$::eid\t$ord\t$kw\n"; } }
 
-sub do_misc { my ($misc) = @_;
+sub do_misc { my ($misc, $ord) = @_;
 	my ($i, $kw, $txt);
 	foreach $i (@$misc) {
 	    $txt = $i->text;
 	    ($kw = $::JM2ID{MISC}{$txt}) or \
 		die ("Unknown \'misc\' text: /$txt/\n");
-	    print $::Fmisc "$::sid\t$kw\n"; } }
+	    print $::Fmisc "$::eid\t$ord\t$kw\n"; } }
 
-sub do_fld { my ($fld) = @_;
+sub do_fld { my ($fld, $ord) = @_;
 	my ($i, $kw, $txt);
 	foreach $i (@$fld) {
 	    $txt = $i->text;
 	    ($kw = $::JM2ID{FLD}{$txt}) or \
 		die ("Unknown \'fld\' text: /$txt/\n");
-	    print $::Ffld "$::sid\t$kw\n"; } }
+	    print $::Ffld "$::eid\t$ord\t$kw\n"; } }
 
 sub do_dial { my ($dial) = @_;
 	my ($i, $kw, $txt);
@@ -301,23 +320,24 @@ sub do_restr { my ($file, $restr, $amap, $bmap) = @_;
 	    foreach $b (sort (values (%$bmap))) {
 		next if (!($r = $restr->{$a}));
 		if ($r == 1 or !$r->{$b}) { 
-		    print $file "$a\t$b\n"; } } } }
+		    print $file "$::eid\t$a\t$b\n"; } } } }
 
-sub do_xref { my ($xtyp, $xref) = @_;
+sub do_xref { my ($xtyp, $xref, $ord) = @_;
 	my ($x, $txt, $kw);
 	$kw = $::JM2ID{XREF}{$xtyp};
 	foreach $x (@$xref) {
 	    $txt = $x->text;
-	    # (id,sens,ord,lang,txt,notes)
-	    print $::Fxref "$::sid\t$kw\t$txt\n"; } }
+	    # (entr,sens,lang,txt,notes)
+	    print $::Fxref "$::eid\t$ord\t$kw\t$txt\n"; } }
 
 sub do_hist { my ($hist) = @_;
 	my ($x, $dt, $op);
 	foreach $x (@$hist) {
 	    $dt = ($x->get_xpath ("upd_date"))[0]->text; # Assume just one.
+	    $dt .= " 00:00:00-00";  # Add a time.
 	    $op = ($x->get_xpath ("upd_detl"))[0]->text; # Assume just one.
 	    if ($op eq "Entry created") {
-		# (id,entr,stat,dt,who,notes)
+		# (id,entr,stat,dt,who,diff,notes)
 		print $::Fhist "$::hid\t$::eid\t2\t$dt\tfrom JMdict.xml\t\\N\t\\N\n"; }
 	    else { die ("Unexpected <upd_detl> contents: $op"); }
 	    $::hid += 1; } }
@@ -338,6 +358,17 @@ sub parse_freq { my ($fstr) = @_;
 	$val = int ($2);
 	return ($kw, $val); }
 
+sub pgesc { my ($str) = @_; 
+	# Escape characters that are special to the Postgresql COPY
+	# command.  Backslash characters are replaced by two backslash
+	# characters.   Newlines are replaced by the two characters
+	# backslash and "n".  Similarly for tab and return characters.
+	$str =~ s/\\/\\\\/go;
+	$str =~ s/\n/\\n/go;
+	$str =~ s/\r/\\r/go;
+	$str =~ s/\t/\\t/go;
+	return $str; }
+
 sub usage { my ($exitstat) = @_;
 	print <<EOT;
 
@@ -353,6 +384,8 @@ Options:
 	-c entry-count -- Number of entries to process.
 	-s start-seq-num -- Sequence number of first entry to process.
 	-k -- (keep) do not delete temporary files.
+	-e encoding -- Ecoding to use when writing messages to stderr
+	    and stdout.  Default is "utf-8".
 	-h -- (help) print this text and exit.
 EOT
 	exit $exitstat; }

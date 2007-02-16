b0d061bcca977d3f17fce00a47dfe89a4195c4d6
stuart <none@none>
2007-02-16 13:18:13 -0700

    Fix typo and tweak readability in GPL  notice.

--- pg/xresolv.sql
+++ pg/xresolv.sql
@@ -13,17 +13,20 @@
 -- an xref to every sense in the target entries.
 
 -------------------------------------------------------------------------
---  This file is part of JMdictDB.  
+--  This file is part of JMdictDB. 
+--
 --  JMdictDB is free software; you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License, or
---  (at your option) any later version.
---   JMdictDB is distributed in the hope that it will be useful,
+--  it under the terms of the GNU General Public License as published 
+--  by the Free Software Foundation; either version 2 of the License, 
+--  or (at your option) any later version.
+--
+--  JMdictDB is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU General Public License for more details.
+--
 --  You should have received a copy of the GNU General Public License
---  along with Foobar; if not, write to the Free Software Foundation, Inc.,
+--  along with JMdictDB; if not, write to the Free Software Foundation,
 --  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 --
 --  Copyright (c) 2006,2007 Stuart McGraw 

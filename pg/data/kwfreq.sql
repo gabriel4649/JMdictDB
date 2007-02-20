9d3256cd69e4cb50d56454226aa0760e07449aa7
stuart <none@none>
2007-02-19 17:58:49 -0700

    Remove "jdd" keyword per jb post to edict email list, 2007-02-19.

--- pg/data/kwfreq.sql
+++ pg/data/kwfreq.sql
@@ -20,7 +20,6 @@
 
 INSERT INTO kwfreq(id,kw,descr) VALUES(1,'ichi',NULL);
 INSERT INTO kwfreq(id,kw,descr) VALUES(2,'gai',NULL);
-INSERT INTO kwfreq(id,kw,descr) VALUES(3,'jdd',NULL);
 INSERT INTO kwfreq(id,kw,descr) VALUES(4,'spec',NULL);
 INSERT INTO kwfreq(id,kw,descr) VALUES(5,'nf',NULL);
 INSERT INTO kwfreq(id,kw,descr) VALUES(6,'gA','2007-01-14 Google counts (Kale Stutzman)');

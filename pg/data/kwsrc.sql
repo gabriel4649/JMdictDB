33be55631503ea5566a4bf92cfa32ae7cfc66e04
stuart <none@none>
2007-04-14 15:06:47 -0600

    Change id of examples from 203 (which effectively inactivated it)   to 3 (maaking it active.)  For the implementation for Jim Breen,   this will interactively changed back to 203.

--- pg/data/kwsrc.sql
+++ pg/data/kwsrc.sql
@@ -20,6 +20,5 @@
 
 INSERT INTO kwsrc(id,kw,descr) VALUES(1,'jmdict','Entry from the JMdict file');
 INSERT INTO kwsrc(id,kw,descr) VALUES(2,'jmnedict','Entry from the JMnedict (names) file');
--- 
-INSERT INTO kwsrc(id,kw,descr) VALUES(203,'examples','Entry from the Examples_s file');
+INSERT INTO kwsrc(id,kw,descr) VALUES(3,'examples','Entry from the Examples_s file');
 

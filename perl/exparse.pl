2fc4db6856ca3e526c53ead24f7b5a53533c4f63
stuart <none@none>
2007-04-14 14:50:24 -0600

    Renamed from exload.pl.  Instead of writing examples entries and   cross-refs directly to the database, exparse will generate a   intermediate .pgi file that uses Postgresql COPY commands and   can be loaded into the database bt jmload.pl much faster.   The previous version took 12+ hours to load the 8MB Examples   file on a 800MHz desktop PC.  It now loads in 12+2+30m (with   indexes and foreign keys still in place).

--- perl/exparse.pl
+++ perl/exparse.pl
@@ -26,28 +26,29 @@
 # The file can be loaded into a Postgresql jmdict database
 # after subsequent processing with jmload.pl.
 
-use strict; 
-use XML::Twig; use Encode; use DBI;
+use strict;  use warnings;
+use Encode; use DBI;
 use Getopt::Std ('getopts');
 
 BEGIN {push (@INC, "./lib");}
-use jmdict;  use jmdictxml ('%EX2ID');  
+use jmdict;  use jmdictxml ('%EX2ID'); use jmdictpgi;
 
 use Memoize;
 memoize ('findentr');
 
-%::Msgs;
+%::Msgs = ();
 $::Debug{prtsql} = 1;
 
 main: {
 	my ($dbh, $infn, $outfn, $enc, $user, $pw, $dbname, $host,
-	    $begin, $count);
+	    $begin, $count, $tmp);
 
-	if (!getopts ("b:c:e:wxvu:p:r:d:h", \%::Opts)) { usage (1); }
+	if (!getopts ("o:b:c:e:knvu:p:r:d:h", \%::Opts)) { usage (1); }
 	if ($::Opts{h}) { usage (0); }
 
 	  # Set some local variables based on the command line 
 	  # options given or defaults where options not given.
+	$outfn =  $::Opts{o} || "examples.pgi";
 	$begin =  $::Opts{b} || 0;
 	$count =  $::Opts{c} || 9999999;
 	$enc =    $::Opts{e} || "utf-8";
@@ -58,7 +59,11 @@ main: {
 
 	binmode(STDOUT, ":encoding($enc)");
 	binmode(STDERR, ":encoding($enc)");
-	eval { binmode($DB::OUT, ":encoding($enc)"); };
+	{no warnings;
+	eval { binmode($DB::OUT, ":encoding($enc)"); }; }
+	  # Make STDERR unbuffered so we print "."s, one at 
+	  # a time, as a sort of progress bar.  
+	$tmp = select(STDERR); $| = 1; select($tmp);
 
 	($infn = shift (@ARGV)) || die ("No input filename given\n");
 
@@ -68,17 +73,17 @@ main: {
 			{ PrintWarn=>0, RaiseError=>1, AutoCommit=>0 } ); 
 	    $dbh->{pg_enable_utf8} = 1; }
 
-	process ($dbh, $infn, $begin, $count);
+	process ($dbh, $infn, $outfn, $begin, $count);
 	msg_summary (\%::Msgs);
 	if (!$::Opts{w}) { $dbh->disconnect(); } }
 	
-sub process { my ($dbh, $infn, $begin, $count) = @_;
-	my ($ln, $aln, $bln, $jtxt, $etxt, $kwds, $cntr, $idxlist); 
+sub process { my ($dbh, $infn, $outfn, $begin, $count) = @_;
+	my ($ln, $aln, $bln, $jtxt, $etxt, $kwds, $cntr, $idxlist, $entr, $tmpfiles, $eid); 
 	open (FIN, "<:utf8", $infn) || die ("Can't open $infn: $!\n");
+	if (!$::Opts{n}) { $tmpfiles = initialize (); }
 	while ($aln = <FIN>) {
 	    next if ($. < $begin);
-	    if ($. == 1 and ((0 >= substr ($aln, 0, 1) eq "\x{FEFF}") or
-			     (0 >= substr ($aln, 0, 1) eq "\x{FFFE}"))) {
+	    if ($. == 1 and (substr ($aln, 0, 1) eq "\x{FEFF}")) {
 		$aln = substr ($aln, 1); }
 	    next if (substr ($aln, 0, 2) ne "A:");
 	    $::Lnnum = $.; $bln = <FIN>;  ++$cntr;
@@ -90,23 +95,22 @@ sub process { my ($dbh, $infn, $begin, $count) = @_;
 	    if ($@) { 
 		chomp ($@); msg ($@); next; }
 	    if (!$::Opts{w}) {	# w--parse only option
-		addindexes ($dbh, $jtxt, $etxt, $kwds, $::Lnnum, $idxlist);
-		if (!$::Opts{w} && !$::Opts{x} && !($cntr % 500 == 0)) {
-		    $dbh->commit(); } }
-	    last if ($cntr >= $count);} 
-	if (!$::Opts{w} && !$::Opts{x}) { $dbh->commit(); } }
+		$entr = entrxref ($dbh, $jtxt, $etxt, $kwds, $::Lnnum, $idxlist);
+		if (!$::Opts{n} && $entr) { setkeys ($entr, ++$eid); wrentr ($entr); } }
+	    if (0 == $cntr % 2000) { print STDERR "."; }
+	    last if ($cntr >= $count);}
+	if ($cntr >= 2000) {print STDERR "\n"; }
+	if (!$::Opts{n}) { finalize ($outfn, $tmpfiles, !$::Opts{k}); } }
 
-
-sub addindexes { my ($dbh, $jtxt, $etxt, $kwds, $lnnum, $idxlist) = @_;
+sub entrxref { my ($dbh, $jtxt, $etxt, $kwds, $lnnum, $idxlist) = @_;
 	# $idxlist data: [(ktxt,rtxt,sens,atxt),...]
 	my ($xrefs, $entr, $src);
 	$xrefs = resolve ($dbh, $idxlist);
 	$xrefs = prunedups ($xrefs);
 	if (scalar (@$xrefs) == 0) {
-	    msg ("No indexable words, line skipped");  return;}
+	    msg ("No indexable words, line skipped");  return undef;}
 	$entr = mkentr ($jtxt, $etxt, $kwds, $lnnum, $xrefs);
-	if (!$::Opts{x}) { 
-	    addentr ($dbh, $entr); } }
+	return $entr; } 
 
 sub parsea { my ($aln) = @_;
 	my ($j, $e, $ntxt, @ntxts, $nt, $kw, @kw);
@@ -215,12 +219,13 @@ sub findentr { my ($dbh, $ktxt, $rtxt) = @_;
 
 sub mkentr { my ($jtxt, $etxt, $kwds, $lnnum, $xrefs) = @_;
 	my ($e, @kws, $snote);
-	my $e = {src=>3, stat=>2, seq=>$lnnum};
+	$e = {src=>3, stat=>2, seq=>$lnnum};
 	if (@$kwds) {
 	    # Each @$kwds item is a 2-array consisting of the kw
 	    # id number and optionally a note string.
 	    @kws = map ($_->[0], @$kwds);
-	    $snote = join ("; ", map ($_->[1], @$kwds)) || undef; }
+	    {no warnings qw(uninitialized);
+	    $snote = join ("; ", map ($_->[1], @$kwds)) || undef; } }
 	$e->{_kanj} = [{txt=>$jtxt}];
 	$e->{_sens} = [{_gloss=>[{lang=>1, txt=>$etxt}],
 		        _misc=>[map ({kw=>$_}, @kws)],
@@ -262,25 +267,30 @@ Arguments:
 	filename -- Name of examples file.
 
 Options:
+	-o filename -- Name of output file.  Default is 
+	    examples.pgi.
 	-b number -- Starting line number in examples file.  
 	    Processing will start at the first "A" line at of
 	    after this.
 	-c number -- Maximum number of example pairs to process.
-	-w -- Parse only.  Do access database at all: do not write
-	    anything to it, nor resolve index words from it.
-	-x -- Resolve only.  Access database read-only in order to 
-	    resolve index words; do not write anything to it.
+	-n -- Parse only, no database access used: do not resolve
+	    index words from it.
 	-v -- Verbose.  Print messages to stderr as irregularies 
 	    are encountered.  With or without this option, the
 	    program will print a full accounting of irregularies
 	    (in a more convenient form) to stdout before it exits.
-	-d dbname -- Name of database to use.  Default is "jmdict".
-	-r host	-- Name of machine hosting the database.  Default
-	    is "localhost".
 	-e encoding -- Encoding to use for stdout and stderr.
 	    Default is "utf-8".  Windows users running with 
 	    a Japanese locale may wish to use "cp932".
+	-k (keep) Do not delete intermediate files when done.
 	-h -- (help) print this text and exit.
+
+	The following options say what database to connect to and 
+	how to connect.
+
+	-d dbname -- Name of database to use.  Default is "jmdict".
+	-r host	-- Name of machine hosting the database.  Default
+	    is "localhost".
 
 	***WARNING***
 	The following two options are not recommended when running 

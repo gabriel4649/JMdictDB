c2621db5c66f8d954219591f77ce16ec992e46d8
stuart <none@none>
2007-04-14 23:07:04 -0600

    Change to load .csv files instead of .sql files.

--- pg/loadkw.sql
+++ pg/loadkw.sql
@@ -1,11 +1,11 @@
-\i data/kwdial.sql
-\i data/kwfreq.sql
-\i data/kwfld.sql
-\i data/kwkinf.sql
-\i data/kwlang.sql
-\i data/kwmisc.sql
-\i data/kwpos.sql
-\i data/kwrinf.sql
-\i data/kwsrc.sql
-\i data/kwstat.sql
-\i data/kwxref.sql
+\copy kwdial from '../pg/data/kwdial.csv' CSV delimiter E'\t'
+\copy kwfreq from '../pg/data/kwfreq.csv' CSV delimiter E'\t'
+\copy kwfld  from '../pg/data/kwfld.csv'  CSV delimiter E'\t'
+\copy kwkinf from '../pg/data/kwkinf.csv' CSV delimiter E'\t'
+\copy kwlang from '../pg/data/kwlang.csv' CSV delimiter E'\t'
+\copy kwmisc from '../pg/data/kwmisc.csv' CSV delimiter E'\t'
+\copy kwpos  from '../pg/data/kwpos.csv'  CSV delimiter E'\t'
+\copy kwrinf from '../pg/data/kwrinf.csv' CSV delimiter E'\t'
+\copy kwsrc  from '../pg/data/kwsrc.csv'  CSV delimiter E'\t'
+\copy kwstat from '../pg/data/kwstat.csv' CSV delimiter E'\t'
+\copy kwxref from '../pg/data/kwxref.csv' CSV delimiter E'\t'

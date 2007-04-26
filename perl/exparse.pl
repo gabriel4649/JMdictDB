aa508d656c678f002b65a387525c66dda5ac212a
stuart <none@none>
2007-04-26 12:31:20 -0600

    Formerly, exparse would try to resolve the B line items against   jmdict entries while running.  Changed so that it now, like   jmparse.pl, doesn't resolve but writes the unresolved text   strings to table xresolv from where they will be resolved   later.  Since the Examples file identifies xref targets by   possibly a reading, kanji, and sense number, the xresolv table   was expanded with columns "rtxt", "ktxt", and "xsens" replacing   the single "txt" column.  Also added a "ord" column since the   the B line items may be repeated and order matched order of   aperance in the A line text. Since database access no longer needed for xref resolution, all   the database stuff was ripped out.

--- perl/exparse.pl
+++ perl/exparse.pl
@@ -33,47 +33,29 @@
 # sense note if there was a parsable "[...]" comment on the
 # line.
 #
-# "B" line items create database xref table rows.  The kanji,
-# reading, or kanji/reading pair text is resolved by looking
-# in the database for matching entries from jmdict.  If no
-# matching entry is found, a warning is entered in the log
-# file and no xref genered for that B line item.  If multiple
-# entries are found, and a warning is genereated and multiple
-# xref's created.  If the B line item has no senses listed 
-# and the target entry has multiple senses, and xref is 
-# generated for each.  A warning is printed and no xref
-# created if a sence is specified that doesn't existing in
-# the target entry.
-# All xrefs have a typ=5 and the (entr,sens) fk points to the
-# example sentence entry, and the (xentr,xsens) fk points to 
-# jmdict entry.
+# "B" line items create database xref table rows.  However,
+# like jmparse, we do not create the xrefs directly from 
+# within this program, but instead write pseudo-xref records
+# that contain the target reading and kanji text, to the 
+# xrslv table, and generate the resolved xrefs later by
+# running insert queries based on joins of xrslv and the
+# jmdict entries.  All the pseudo-xref genereated by this
+# program will have a typ=6.
 
 use strict;  use warnings;
-use Encode; use DBI;
+use Encode; 
 use Getopt::Std ('getopts');
 
 BEGIN {push (@INC, "./lib");}
-use jmdict;  use jmdictxml ('%EX2ID'); use jmdictpgi;
-
-# Many of the B line references occur over and over again.
-# To avoid repeatedly looking them up in the database, memoize
-# the function that does the lookups (findentr).  The memoize
-# code will cache the call arguments and results, and when the 
-# same arguments are seen again later, will return the results
-# from cache rather then doing another timeconsuming database
-# query.
- 
-use Memoize;
-memoize ('findentr');
+use jmdict;  use jmdictxml ('%EX2ID'); use jmdictpgi; use kwstatic;
 
 %::Msgs = ();
 $::Debug{prtsql} = 1;
 
 main: {
-	my ($dbh, $infn, $outfn, $enc, $user, $pw, $dbname, $host,
-	    $begin, $count, $tmp);
+	my ($infn, $outfn, $enc, $begin, $count, $tmp);
 
-	if (!getopts ("o:b:c:e:knvu:p:r:d:h", \%::Opts)) { usage (1); }
+	if (!getopts ("o:b:c:e:knvh", \%::Opts)) { usage (1); }
 	if ($::Opts{h}) { usage (0); }
 
 	  # Set some local variables based on the command line 
@@ -82,10 +64,6 @@ main: {
 	$begin =  $::Opts{b} || 0;
 	$count =  $::Opts{c} || 9999999;
 	$enc =    $::Opts{e} || "utf-8";
-	$user =   $::Opts{u} || "postgres";
-	$pw =     $::Opts{p} || "";
-	$dbname = $::Opts{d} || "jmdict";
-	$host =   $::Opts{r} || "";
 
 	binmode(STDOUT, ":encoding($enc)");
 	binmode(STDERR, ":encoding($enc)");
@@ -97,21 +75,12 @@ main: {
 
 	($infn = shift (@ARGV)) || die ("No input filename given\n");
 
-	if (!$::Opts{w}) {
-	    if ($host) { $host = ";host=$host"; }
-	    $dbh = DBI->connect("dbi:Pg:dbname=$dbname$host", $user, $pw, 
-			{ PrintWarn=>0, RaiseError=>1, AutoCommit=>0 } ); 
-	    $dbh->{pg_enable_utf8} = 1; }
+	process ($infn, $outfn, $begin, $count);
+	msg_summary (\%::Msgs); }
 
-	process ($dbh, $infn, $outfn, $begin, $count);
-	msg_summary (\%::Msgs);
-	if (!$::Opts{w}) { $dbh->disconnect(); } }
-	
-sub process { my ($dbh, $infn, $outfn, $begin, $count) = @_;
+sub process { my ($infn, $outfn, $begin, $count) = @_;
 
 	# Process the Examples file.  
-	# $dbh -- Open database handle we will use when resolving
-	#    the B line cross-references.
 	# $infn -- Name of ther input Examples file.
 	# $outfn -- Name of the output .pgi file.
 	# $begin -- Line number at which to begin processing.  Everythig
@@ -130,30 +99,18 @@ sub process { my ($dbh, $infn, $outfn, $begin, $count) = @_;
 	    $::Lnnum = $.; $bln = <FIN>;  ++$cntr;
 	    $aln =~ s/[\r\n]+$//;  $bln =~ s/[\r\n]+$//;
 	    ($jtxt, $etxt, $kwds) = eval { parsea ($aln); };
-	    if ($@) { 
-		chomp ($@); msg ($@); next; }
+	    if ($@) { chomp ($@); msg ($@); next; }
 	    $idxlist = eval { parseb ($bln, $jtxt); };
-	    if ($@) { 
-		chomp ($@); msg ($@); next; }
+	    if ($@) { chomp ($@); msg ($@); next; }
+	    $entr = mkentr ($jtxt, $etxt, $kwds, $::Lnnum);
+	    $entr->{_sens}[0]{_xrslv} = mkxrslv ($idxlist);
 	    if (!$::Opts{w}) {	# w--parse only option
-		$entr = entrxref ($dbh, $jtxt, $etxt, $kwds, $::Lnnum, $idxlist);
-		if (!$::Opts{n} && $entr) { 
-		    setkeys ($entr, ++$eid); 
-		    wrentr ($entr); } }
+		setkeys ($entr, ++$eid); 
+		wrentr ($entr); } 
 	    if (0 == $cntr % 2000) { print STDERR "."; }
-	    last if ($cntr >= $count);}
-	if ($cntr >= 2000) {print STDERR "\n"; }
+	    last if ($cntr >= $count); }
+	if ($cntr >= 2000) { print STDERR "\n"; }
 	if (!$::Opts{n}) { finalize ($outfn, $tmpfiles, !$::Opts{k}); } }
-
-sub entrxref { my ($dbh, $jtxt, $etxt, $kwds, $lnnum, $idxlist) = @_;
-	# $idxlist data: [(ktxt,rtxt,sens,atxt),...]
-	my ($xrefs, $entr, $src);
-	$xrefs = resolve ($dbh, $idxlist);
-	$xrefs = prunedups ($xrefs);
-	if (scalar (@$xrefs) == 0) {
-	    msg ("No indexable words, line skipped");  return undef;}
-	$entr = mkentr ($jtxt, $etxt, $kwds, $lnnum, $xrefs);
-	return $entr; } 
 
 sub parsea { my ($aln) = @_;
 	my ($j, $e, $ntxt, @ntxts, $nt, $kw, @kw);
@@ -174,10 +131,10 @@ sub parseb { my ($bln, $jtxt) = @_;
 	die ("Expected \"B\" line, got '$bln'\n") if (shift(@parts) ne "B:");
 	foreach $x (@parts) {
 	    next if ($x =~ m/^\s*$/);
-	    push (@res, bpart ($x, ++$n, $jtxt)); } 
+	    push (@res, parsebitem ($x, ++$n, $jtxt)); } 
 	return \@res; }
 
-sub bpart { my ($s, $n, $jtxt) = @_;
+sub parsebitem { my ($s, $n, $jtxt) = @_;
 	my ($ktxt, $rtxt, $sens, $atxt, @sens);
 	die ("\"B\" line parse error in item $n: '$s'\n")
 	    if (!($s =~ m/^([^([{]+)(\((\S+)\))?(\[\d+\])*(\{(\S+)\})?\s*$/));
@@ -191,78 +148,15 @@ sub bpart { my ($s, $n, $jtxt) = @_;
 	die ("\{$atxt\} not in A line in item $n\n") if ($atxt && index ($jtxt, $atxt) < 0);
 	return [$ktxt,$rtxt,\@sens,$atxt]; }
 
-sub resolve { my ($dbh, $idxlist) = @_;
-	my ($idx, $ktxt, $rtxt, $sens, $atxt, $targs, @teids, $teid, 
-	    $tsens, @xrefs, $xr, $etxt);
-	foreach $idx (@$idxlist) {
-	    ($ktxt, $rtxt, $sens, $atxt) = @$idx;
-	    $targs = findentr ($dbh, $ktxt, $rtxt);
-	    @teids = keys(%$targs);  
-	    $etxt = hw ($ktxt, $rtxt);
-	    if (scalar(@teids) == 0) {
-		msg ("Warning: Unresolved: $etxt"); next; }
-	    if (scalar(@teids) > 1) {
-		msg ("Warning: Multipliy resolved: $etxt"); next; }
-	    $teid = $teids[0]; $tsens = $targs->{$teid};
-	    
-	    $xr = mkxrefs ($teid, $tsens, $sens, $atxt, $etxt);
-	    push (@xrefs, @$xr); }
-	return \@xrefs; }
-
-sub mkxrefs { my ($teid, $tsens, $sens, $atxt, $etxt) = @_;
-	# Generate xref structs to senses @sens in target entry $teid.
-	#   $teid -- Entry id of target entry.
-	#   $tsens -- List of sense numbers in target entry.
-	#   $sens -- List if sensee numbers to generate xrefs to,
-	#       If false, generate to all target senses.
-	#   $atxt -- Xref note text.
-	#   $etxt -- Text stringt for use in warning message to
-	#       identify target entry.
-
-	my ($s, $ts, $found, @xrefs);
-	foreach $s (@$sens) {
-	    $found = 0; 
-	    foreach $ts (@$tsens) {
-		next if ($ts != $s);
-		push (@xrefs, {entr=>0, sens=>0, xentr=>$teid, xsens=>$ts, typ=>5, notes=>$atxt}); 
-		$found = 1; last; }
-	    if (!$found) { 
-		msg ("no Sense $s in $etxt"); } }
-	if (!@$sens) {
-	    foreach $ts (@$tsens) {
-	        push (@xrefs, {entr=>0, sens=>0, xentr=>$teid, xsens=>$ts, typ=>5, notes=>$atxt}); } }
-	return \@xrefs; }
-
 sub hw { my ($ktxt, $rtxt) = @_;
 	if ($ktxt && $rtxt) { return "$ktxt($rtxt)"; }
 	return $ktxt || $rtxt; }
 
-sub findentr { my ($dbh, $ktxt, $rtxt) = @_;
-	my (@f, @w, @a, $fs, $ws, $sth, $sql, $rs, $r, %eids);
-	die ("both args are zilch") if (!$ktxt and !$rtxt);
-	if ($ktxt && $rtxt) {
-	    $fs = "JOIN kanj k ON k.entr=e.id JOIN rdng r ON r.entr=e.id";
-	    $ws = "k.txt=? AND r.txt=?";  @a = ($ktxt, $rtxt); }
-	elsif ($ktxt) {
-	    $fs = "JOIN kanj k ON k.entr=e.id";
-	    $ws = "k.txt=?";  @a = ($ktxt); }
-	elsif ($rtxt) {
-	    $fs = "JOIN rdng r ON r.entr=e.id LEFT JOIN kanj k ON k.entr=e.id ";
-	    $ws = "r.txt=? AND k.txt IS NULL";  @a = ($rtxt); }
-	$sql = "SELECT id,sens FROM entr e JOIN sens s ON s.entr=e.id $fs WHERE src=1 AND $ws";
-	$sth = $dbh->prepare_cached ($sql);
-	$sth->execute (@a);
-	$rs = $sth->fetchall_arrayref();
-	foreach $r (@$rs) { 
-	    # Build hash id eid's with values being list of 
-	    # sense numbers in that id.
-	    if (!$eids{$r->[0]}) { $eids{$r->[0]} = [$r->[1]]; }
-	    else { push (@{$eids{$r->[0]}}, $r->[1]); } }
-	return \%eids; }
-
-sub mkentr { my ($jtxt, $etxt, $kwds, $lnnum, $xrefs) = @_;
+sub mkentr { my ($jtxt, $etxt, $kwds, $lnnum) = @_;
+	# Create an entry object to represent the "A" line text of the 
+	# example sentence.
 	my ($e, @kws, $snote);
-	$e = {src=>3, stat=>2, seq=>$lnnum};
+	$e = {src=>$KWSRC_examples, stat=>$KWSTAT_A, seq=>$lnnum};
 	if (@$kwds) {
 	    # Each @$kwds item is a 2-array consisting of the kw
 	    # id number and optionally a note string.
@@ -273,16 +167,31 @@ sub mkentr { my ($jtxt, $etxt, $kwds, $lnnum, $xrefs) = @_;
 	$e->{_sens} = [{_gloss=>[{lang=>1, txt=>$etxt}],
 		        _misc=>[map ({kw=>$_}, @kws)],
 			notes=>$snote,},];
-	     $e->{_sens}[0]{_xref} = $xrefs;
 	return $e; }
 
-sub prunedups { my ($xrefs) = @_;
-	# Eliminate any duplicate (i.e., with the same pk) xrefs.
- 	my (%hash, $x, $pk, @res);
-	foreach $x (@$xrefs) {
-	    $pk = "$x->{entr}_$x->{sens}_$x->{xentr}_$x->{xsens}_$x->{typ}";
-	    if (!$hash{$pk}) { push (@res, $x); $hash{$pk} = 1; } }
-	return \@res; }
+sub mkxrslv { my ($idxlist) = @_;
+	# Convert the $@indexlist that was created by bparse() into a 
+	# list of database xrslv table records.  The fk fields "entr"
+	# and "sens" are not set in the xrslv records; they are set
+	# by setids() just prior to writing to the database.
+
+	my ($x, @r, $s, $ktxt, $rtxt, $senslst, $note);
+	foreach $x (@$idxlist) {
+	    ($rtxt, $ktxt, $senslst, $note) = @$x;
+	    if (@$senslst) {
+		# A list of explicit sens were give in the B line, 
+		# create an xrslv record for each.
+		foreach $s (@$senslst) {
+	    	    push (@r, {ktxt=>$x->[0], rtxt=>$x->[1], tsens=>$s,
+				typ=>$KWXREF_uses, notes=>$x->[3]}); } }
+	    else {
+		# This is not list of senses so this cross-ref will 
+		# apply to all the target's senses.  Don't set a "sens"
+		# field in the xrslv record will will result in a NULL
+		# in the database record.
+		push (@r, {ktxt=>$x->[0], rtxt=>$x->[1], 
+			    typ=>$KWXREF_uses, notes=>$x->[3]}); } }
+	return \@r; }
 
 sub kana_only { my ($txt) = @_; 
 	my $v = jstr_classify ($txt);
@@ -328,21 +237,5 @@ Options:
 	-k (keep) Do not delete intermediate files when done.
 	-h -- (help) print this text and exit.
 
-	The following options say what database to connect to and 
-	how to connect.
-
-	-d dbname -- Name of database to use.  Default is "jmdict".
-	-r host	-- Name of machine hosting the database.  Default
-	    is "localhost".
-
-	***WARNING***
-	The following two options are not recommended when running 
-	on a multi-user machine since their values will be visible
-	to anyone who can run a \"ps\" command.
-
-	-u username -- Username to use when connecting to database.
-	    Default is "postgres".
-	-p password -- Password to use when connecting to database.
-	    No default.
 EOT
 	exit $exitstat; }

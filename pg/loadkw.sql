6e5255999ea3abc349bb829fae952d61c3ce1152
stuart <none@none>
2007-05-21 13:56:24 -0600

    Add the new tables kwginf, kwlsrc. {33}

--- pg/loadkw.sql
+++ pg/loadkw.sql
@@ -1,8 +1,11 @@
+\set ON_ERROR_STOP 1
 \copy kwdial from '../pg/data/kwdial.csv' CSV delimiter E'\t'
 \copy kwfreq from '../pg/data/kwfreq.csv' CSV delimiter E'\t'
 \copy kwfld  from '../pg/data/kwfld.csv'  CSV delimiter E'\t'
+\copy kwginf from '../pg/data/kwginf.csv' CSV delimiter E'\t'
 \copy kwkinf from '../pg/data/kwkinf.csv' CSV delimiter E'\t'
 \copy kwlang from '../pg/data/kwlang.csv' CSV delimiter E'\t'
+\copy kwlsrc from '../pg/data/kwlsrc.csv' CSV delimiter E'\t'
 \copy kwmisc from '../pg/data/kwmisc.csv' CSV delimiter E'\t'
 \copy kwpos  from '../pg/data/kwpos.csv'  CSV delimiter E'\t'
 \copy kwrinf from '../pg/data/kwrinf.csv' CSV delimiter E'\t'

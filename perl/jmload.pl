65e16efc2762ffb199b581ba2ec95a109dc7c688
stuart <none@none>
2007-07-13 17:00:11 -0600

    New options -m and -x to support the change to fixed entr.id   numbers. {127} Process files containing fixed entr.id numbers (now produced by   jmparse.pl and exparse.pl, or ordinary pgdump files) rather than   requiring the symbolic entr.id values used previously. {126}

--- perl/jmload.pl
+++ perl/jmload.pl
@@ -21,10 +21,10 @@
 @VERSION = (substr('$Revision$',11,-2), \
 	    substr('$Date$',7,-11));
 
-# This program will adjust the relative eid numbers in the .pgi
-# file produced by jmload.pl to fixed values for a specific database.
-# The .dmp file produced by this program cam then be loaded into
-# that database.
+# This program will adjust the entr id numbers in a postgresql
+# dump file of a jmdict database in order to make it loadable
+# into a database where the file's entr id numbers are already
+# in use.
 
 use strict; 
 use Encode; use DBI;
@@ -34,25 +34,28 @@ BEGIN {push (@INC, "./lib");}
 use jmdictxml ('%JM2ID'); # Maps xml expanded entities to kw* table id's.
 
 main: {
-	my ($infn, $outfn, $enc, $eid, $user, $pw, $dbname, $host);
+	my ($infn, $outfn, $enc, $eid, $user, $pw, $dbname, $host, 
+	    $v, $tblnm, $offtbl, $rmin, $rmax, @a, @cols, $col, 
+	    $eiddelt, $delt, @updtd);
 
-	getopts ("o:i:e:u:p:d:r:h", \%::Opts);
+	getopts ("o:i:m:x:e:u:p:d:r:h", \%::Opts);
 	if ($::Opts{h}) { usage (0); }
 	$enc = $::Opts{e} || "utf-8";
 	binmode(STDERR, ":encoding($enc)");
 	eval { binmode($DB::OUT, ":encoding($enc)"); };
 
-	$infn = shift (@ARGV) || "JMdict.pgi";
-	$outfn  = $::Opts{o} || "JMdict.dmp";
+	$infn   = shift (@ARGV) || "jmdict.pgi";
+	$outfn  = $::Opts{o}    || "jmdict.dmp";
+	$rmin   = $::Opts{m}    || 1;
+	$rmax   = $::Opts{x}    || 0;
 
-	$user   = $::Opts{u} || "postgres";
-	$pw     = $::Opts{p} || "";
-	$dbname = $::Opts{d} || "jmdict";
-	$host   = $::Opts{r} || "";
+	$user   = $::Opts{u}    || "postgres";
+	$pw     = $::Opts{p}    || "";
+	$dbname = $::Opts{d}    || "jmdict";
+	$host   = $::Opts{r}    || "";
 
-	if (defined ($::Opts{i})) {
-	    $eid = int($::Opts{i}); }
-	else {
+	$eid    = int($::Opts{i}) || 0;
+	if (!$eid) {
 	      # Get the starting value of $::eid from the
 	      # max values of entr.id found in the database.
 	      # If that database id number changes between the time we
@@ -63,16 +66,63 @@ main: {
 	    $eid = get_max_ids ($user, $pw, $dbname, $host); }
 	if (!$eid =~ m/^-?\d+$/) {
 	    die ("Did not get valid entr.id  value, please check -i\n"); }
-	print STDERR "Initial entr.id = $eid\n";
-	$eid--;  # Adjust for fact that .pgi file numbers start at 1.
+	$eiddelt = $eid - $rmin;
+	print STDERR "Rebasing $rmin" . ($rmax ? "-".$rmax : "") . " to " . 
+	    ($rmin + $eiddelt) . ($rmax ? "-".$rmin+$eiddelt+$rmax : "") ."\n";
 
 	open (FIN, "<:utf8", $infn) or die ("Can't open $infn: $!\n");
 	open (FOUT, ">:utf8", $outfn) or die ("Can't open $outfn: $!\n");
-	while (<FIN>) {
-	    s/\$\$E\$\$(\d+)/$1+$eid/eo;
-	    print FOUT; }
-	print STDERR ("Done\n"); }
 
+	# This hash identifies the database tables that have a foreign
+	# reference to entr.id, and the column(s) (by number) of each 
+	# such.
+	$offtbl = {
+	    entr    => [0],
+	    rdng    => [0],
+	    rinf    => [0],
+	    kanj    => [0],
+	    kinf    => [0],
+	    sens    => [0],
+	    gloss   => [0],
+	    misc    => [0],
+	    pos     => [0],
+	    fld     => [0],
+	    hist    => [0],
+	    dial    => [0],
+	    lsrc    => [0],
+	    ginf    => [0],
+	    restr   => [0],
+	    stagr   => [0],
+	    stagk   => [0],
+	    xref    => [0,2], 
+	    xresolv => [0],
+	    tcard   => [2], 
+	    tsndasn => [1], };
+
+	$delt = 0;
+	while (<FIN>) {
+	    s/[\n\r]+$//o;
+	    if ($. == 1 and (substr ($_, 0, 1) eq "\x{FEFF}")) {
+		$_ = substr ($_, 1); }
+	    if (m/^[\\]?COPY\s+([^\s(]+)/oi) {
+		$tblnm = $1;
+		if ($v = $offtbl->{$tblnm}) { # That's '=', not '=='!
+		    @cols = @{$v};
+		    $delt = $eiddelt;
+		    push (@updtd, $tblnm); } }
+	    elsif (m/^\\\.\s*$/o) {
+		$delt = 0; }
+	    elsif ($delt) {
+		@a = split ("\t");
+		foreach $col (@cols) {
+		    $v = $a[$col];
+		    if ($v >= $rmin && (!$rmax || $v < $rmax)) {
+		        $a[$col] += $delt; } }
+		$_ = join ("\t", @a); } 
+	    print FOUT "$_\n"; }
+	if (@updtd && $eiddelt != 0) {
+	    print STDERR "Rebased tables: " . join (",",@updtd) . "\n"; }
+	else { print STDERR "No changes made\n"; } }
 
 sub get_max_ids { my ($user, $pw, $dbname, $host) = @_;
 	# Get and return 1 + the max values of entr.id found in the
@@ -93,33 +143,46 @@ sub get_max_ids { my ($user, $pw, $dbname, $host) = @_;
 sub usage { my ($exitstat) = @_;
 	print <<EOT;
 
-jmload.pl reads a .pgi such a produced by jmparse.pl and converts it
-into a loadable Postgresql dmp file by converting the relative entry 
-id numbers in the .pgi fie to actual numbers for a specific database.
+jmload.pl reads a postgresql dump file such as produced by 
+jmparse.pl and adjusts the entr.id numbers in it.  This allows
+jmparse.pl and other loader programs to generate entr.id numbers 
+starting at 1, and rely on this program to adjust the numbers 
+prior to loading into a database to avoid duplicate id numbers
+when the database already contains other entries.
+
+It also allows entries to be extracted from a database with
+the postgresql 'copy' command and loaded into another database 
+by rebasing the entr.id numbers.
 
 Usage: jmload.pl [-o output-filename] [-i starting-id-value] \\
+		      [-m minimum-id] [-x maximum-id]
 		      [-u username] [-p password] [-d database] \\
 		      [-r host] [-e encoding] \\
 		    [pgi-filename]
 
 Arguments:
-	pgi-filename -- Name of input file that was created by
-	  jmparse.pl.  Default is "JMdict.pgi".
+	pgi-filename -- Name of input dump file.  Default is 
+	    "jmdict.pgi".
 Options:
 	-h -- (help) print this text and exit.
 	-o output-filename -- Name of output postgresql dump file. 
-	    Default is "JMdict.dmp"
-	-i starting-id -- Starting number for entry id fields.
+	    Default is "jmdict.dmp"
+	-i starting-id -- Value that the minimum entr id (given
+	    by the -m option) will be adjusted to.
 	    It not given, jmload.pl will connect to the database
 	    (using the options below) to read the max id number
-	    from the database and start with that number plus one.
+	    from the database and use that number plus one.
+	-m minimum-id -- Only entr id's equal or greater than this
+	    will be modified.  If not given, default is 1.
+	-x maximum-id -- Only entr id's less than this will be
+	    modified.  If not given or 0, no maximum will apply.
 
-	If -i was not given, the following options will be used to 
-	connect to a database in order to read the max entr.id value.
-        If the dmp file is loaded into a different database, or if
-	the max entr.id value changes between load_jmdict.pl's read
-	and loading the dump file, it is likely duplicate key errors 
-	will occur.
+	   If -i was not given, the following options will be used  
+	   to connect to a database in order to read the max entr.id 
+           value.  If the dmp file is loaded into a different database, 
+	   or if the max entr.id value changes between load_jmdict.pl's 
+	   read and loading the dump file, it is likely duplicate key  
+	   errors will occur.
 
 	-d dbname -- Name of database to use.  Default is "jmdict".
 	-r host	-- Name of machine hosting the database.  Default
@@ -129,10 +192,12 @@ Options:
 		a Japanese locale may wish to use "cp932".
 	-h -- (help) print this text and exit.
 
-	  ***WARNING***
-	  The following two options are not recommended for use 
-	  on a multi-user machine because their values will be 
-	  visible to anyone who can run a \"ps\" command.
+	   ***WARNING***
+	   The following two options are not recommended for use 
+	   on a multi-user machine because their values will be 
+	   visible to anyone who can run a \"ps\" command.  Instead
+	   use a Postgresql password file (see section 29.13. "The
+	   Password File" in the Postgresql docs.)
 
 	-u username -- Username to use when connecting to database.
 	        Default is "postgres".

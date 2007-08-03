0ad1d39deeff686c41b1a92c579c19ecaf12fa72
stuart <none@none>
2007-08-03 16:15:55 -0600

    Move functions fmt_restr(), fmt_stag(), set_audio_flag from   perl/cgi/entr.pl to perl/lib/jmdictcgi.pm since they are also   needed by cgi/edconf.pl. {185}

--- perl/lib/jmdictcgi.pm
+++ perl/lib/jmdictcgi.pm
@@ -41,4 +41,67 @@ use jmdict;
 	$s = thaw ($s);
 	$s; }
 
+    sub fmt_restr { my ($entrs) = @_;
+
+	# In the database we store the invalid combinations of readings
+	# and kanji, but for display, we want to show the valid combinations.
+	# So we subtract the former set which we got from the database from
+	# from the full set of all combinations, to get the latter set for
+	# display.  We also set a HAS_RESTR flag on the entry so that the 
+	# display machinery doesn't have to search all the readings to 
+	# determine if any restrictions exist for the entry.
+
+	my ($e, $nkanj, $r);
+	foreach $e (@$entrs) {
+	    next if (!$e->{_kanj});
+	    $nkanj = scalar (@{$e->{_kanj}});
+	    foreach $r (@{$e->{_rdng}}) {
+		next if (!$r->{_restr});
+		$e->{HAS_RESTR} = 1;
+		if (scalar (@{$r->{_restr}}) == $nkanj) { $r->{_RESTR} = 1; }
+		else {
+		    my @rk = map ($_->{txt}, 
+			@{filt ($e->{_kanj}, ["kanj"], $r->{_restr}, ["kanj"])});
+		    $r->{_RESTR} = \@rk; } } } }
+
+    sub fmt_stag { my ($entrs) = @_; 
+
+	# Combine the stagr and stagk restrictions into a single
+	# list, which is ok because former show in kana and latter
+	# in kanji so there is no interference.  We also change
+	# from the "invalid combinations" stored in the database
+	# to "valid combinations" needed for display.
+
+	my ($e, $s);
+	foreach $e (@$entrs) {
+	    foreach $s (@{$e->{_sens}}) {
+		my @stag;
+		if ($s->{_stagk} and @{$s->{_stagk}}) {
+		    push (@stag, map ($_->{txt},
+		      @{filt ($e->{_kanj}, ["kanj"], $s->{_stagk}, ["kanj"])})); }
+		if ($s->{_stagr} and @{$s->{_stagr}}) {
+		    push (@stag, map ($_->{txt},
+		      @{filt ($e->{_rdng}, ["rdng"], $s->{_stagr}, ["rdng"])})); }
+		$s->{_STAG} = @stag ? \@stag : undef; } } }
+
+    sub set_audio_flag { my ($entrs) = @_; 
+
+	# The display template shows audio records at the entry level 
+	# rather than the reading level, so we set a HAS_AUDIO flag on 
+	# entries that have audio records so that the template need not
+	# sear4ch all readings when deciding if it should show the audio
+	# block.
+	# [Since the display template processes readings prior to displaying
+	# audio records, perhaps the template should set its own global
+	# variable when interating the readings, and use that when showing
+	# an audio block.  That would eliminate the need for this function.]
+ 
+	my ($e, $r, $found);
+	foreach $e (@$entrs) {
+	    $found = 0;
+	    foreach $r (@{$e->{_rdng}}) {
+		if ($r->{_audio}) { $found = 1; last; } }
+	    $e->{HAS_AUDIO} = $found; } }
+
+
 1;

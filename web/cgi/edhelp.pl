e3e5a55babe28287b0c113eb2f24a94c7c447c1c
stuart <none@none>
2007-08-21 16:31:57 -0600

    Add edhelp.pl/tal page that provides entry editing help and refer to   it from entry edit form. {247}

---
+++ web/cgi/edhelp.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+#######################################################################
+#   This file is part of JMdictDB. 
+#   Copyright (c) 2007 Stuart McGraw 
+# 
+#   JMdictDB is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published 
+#   by the Free Software Foundation; either version 2 of the License, 
+#   or (at your option) any later version.
+# 
+#   JMdictDB is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with JMdictDB; if not, write to the Free Software Foundation,
+#   51 Franklin Street, Fifth Floor, Boston, MA  02110#1301, USA
+#######################################################################
+
+@VERSION = (substr('$Revision$',11,-2), \
+	    substr('$Date$',7,-11));
+
+use strict; use warnings; 
+use CGI; use DBI; 
+use Petal; use Petal::Utils; 
+
+BEGIN {push (@INC, "../lib");}
+use jmdict; use jmdicttal;
+
+$|=1;
+binmode (STDOUT, ":utf8");
+
+    main: {
+	my ($dbh, $cgi, $tmpl, @kw, @kwlist, $t );
+	#$cgi = new CGI;
+	print "Content-type: text/html\n\n";
+	$dbh = dbopen ();  $::KW = Kwds ($dbh);
+	$dbh->disconnect; 
+
+	for $t qw(RINF KINF FREQ MISC POS FLD DIAL LANG GINF SRC STAT XREF) {
+	    @kw = kwrecs ($::KW, $t);
+	    push (@kwlist, [ucfirst(lc($t)), 
+		  [sort {lc($a->{kw}) cmp lc($b->{kw})} 
+			grep ($_->{id}<200 || $_->{id}>299,  @kw)]]); }
+
+	$tmpl = new Petal (file=>'../lib/tal/edhelp.tal', 
+			   decode_charset=>'utf-8', output=>'HTML' );
+	print $tmpl->process (kwlist=>\@kwlist); }
+

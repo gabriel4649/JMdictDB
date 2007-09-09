8eeaed63e10e6ce578cd750d5049d0ac305ef49b
stuart <none@none>
2007-09-09 13:50:06 -0600

    Fix: Make svc param safe before using it. {312} Fix: Get rid of "used only once" warnings about $DB::OUT. {311} Rewritten to use new entr table columns to support concurrent edits,   approve, and reject. {316}

--- web/cgi/edsubmit.pl
+++ web/cgi/edsubmit.pl
@@ -18,6 +18,108 @@
 #   51 Franklin Street, Fifth Floor, Boston, MA  02110#1301, USA
 #######################################################################
 
+# In the database entr table rows have three attributes that
+# support entry editing:
+#   dfrm -- Contains the entr.id number of the entry that the 
+#	current entry was derived from.
+#   unap -- Indicates the current entry is not "approved"
+#   stat -- Has value corresponding to one of the kwstat kw's:
+#	N -- New entry
+#	A -- Active entry
+#	D -- Deleted entry
+#	R -- Rejected entry
+#
+# Basics:
+# 1. When an entry is edited and submited a new entry object is
+#    added to the database that represents the object as edited,
+#    but leaves the original entry object in the database as well.
+# 2. The edited entry contains the id number of the entry it was 
+#    derived from in atttribute "dfrm".
+# 3. Any entry can be edited and submitted including previously
+#    edited entries.
+# The result is that there can exist in the database a "tree" of
+# edited entries, linked by "dfrm" values, with the original entry
+# at its root.
+# We define a 'chain" to be a subtree in which no branches exist,
+# that is, each parent entry has only one child.  The "chain head"
+# is the entry on the chain that is nearest to the root. 
+#
+# Parameters:
+# There are two URL parameters processed by this script that 
+# describe the action to be taken:
+#    serialized:
+#	A serialized perl representation of the edited object
+#	 that was given in the edit form.
+#    disp:
+#	"" (or no disp param) -- Submit
+#	"a" -- Approve
+#	"r" -- Reject
+# A submission made by this cgi script always creates a new entry
+# object in the database. ("object" means an entr table row, and
+# related rows in the other tables).
+#
+# A "submit" submission:
+# ---------------------
+# In the case of a "submit" entry (this is the only kind of
+# submission permitted by non-editors), the unpacked entry
+# is used to create a new database object.  If the user submits
+# a new entry: 
+#	stat=N, dfrm=NULL, unap=1
+# If it is edited version ofexisting entry:
+#	stat=A, dfrm=<previous entr.id>, unap=1
+# Related rows are created in other table as needed to create a
+# database representation of the edited entry. 
+# This process adds an entry to the database but makes no changes
+# to existing entries.
+# The history record in the submitter's entry is combined with 
+# the history records from the parent entry to form a complete
+# history for the edited entry.
+#
+# An "approve" submission:
+# -----------------------
+# The root entry is found by following the dfrm links, and then
+# all leaf entries are found.  Each of these is an edit that
+# hasn't been itself edited.  If there is more that one, we
+# refuse to allow the approval of the edit we are processing
+# and require the editor to explicitly reject the other edits
+# first, to ensure that edits aren't inadvertantly missed.
+#
+# If there is only one leaf entry, it must be our parent.
+# We save it's history and then delete the chain containing
+# it, bach to the root entry.
+# A new entry is created as for a "submit" entry except unap=F,
+# and if stat=N, it is changed to A, and dfrm is set to NULL.
+#
+# A "reject" submission
+# ---------------------
+# We make a rejection by creating and entry with stat=R, unap=F,
+# and dfrm=NULL.  We delete the chain containing the parent
+# entry.   This may not go back to the root entry.
+#
+# Concurrency:
+# ------------
+# A long time may pass between when an entry is read for
+# editing by edform.pl and when it is submitted by edsubmit.pl.
+# During this time another user may submit other edits of the
+# same entry, or of one its parents.   
+# An editor may approve or reject edits resulting in the 
+# disappearance of the edited entry's parents.
+# This situation is detected in merge_hist() when it tries 
+# to merge the history records from the parent entry.
+# Like other system that permit concurrent editing (e.g.
+# CVS) we report an edit conflict and require the user
+# to resolve conflicts manually by reediting the entry.
+# 
+# It is also possible that the edit tree could change while
+# edsubmit.pl is running: between the time it is checked for
+# changes but before the edited entry is  written or previous
+# entries deleted.  This is guarded against by doing the
+# checks and updates inside a transaction run with "serializable"
+# isolation.  The database state within the tranaction is
+# garaunteed not to change, and if someone else makes a 
+# conflicting change outside the transaction, the transaction
+# will fail with an error.
+
 @VERSION = (substr('$Revision$',11,-2), \
 	    substr('$Date$',7,-11));
 
@@ -31,68 +133,239 @@ use jmdict; use jmdictcgi; use jmdicttal;
 
 $|=1;
 binmode (STDOUT, ":utf8");
-eval { binmode($DB::OUT, ":encoding(shift_jis)"); };
+{no warnings 'once'; 
+    eval { binmode($DB::OUT, ":encoding(shift_jis)"); }; }
 
     main: {
-	my ($dbh, $cgi, $tmpl, $entr, $entrs, $x, $eid, $seq, $src, @added, $svc);
+	my ($dbh, $cgi, $tmpl, $entr, $entrs, $x, $eid, $seq, $src, @added,
+	    @errs, $svc, $disp);
 	$cgi = new CGI;
 	print "Content-type: text/html\n\n";
-	$svc = $cgi->param ("svc");
+	$svc = clean ($cgi->param ("svc"));
 	$dbh = dbopen ($svc);  $::KW = Kwds ($dbh);
+	$dbh->commit();  $dbh->do ("START TRANSACTION ISOLATION LEVEL SERIALIZABLE");
 
-	# We do little error checking here because the url parameters came 
-	# from one of our pages and should be ok.  If someone synthesizes 
-	# bad url parameters, we feel no obligation to give them nice errors
-	# messages.  Any serious or security problems will be caught be the 
-	# database's integrity checking.  What we do preserve here (because
-	# database cannot check this) is the integrity of the history
-	# records -- the history records transferred to the new entry *must*
-	# come from the old entr (as identified by id received by url) with
-	# only one new one (from the submitter) added.  The sequence number
-	# and source id must also match the original entry.  New new entries
-	# must have only the history record provided by the submitter (not
-	# a bunch of fake ones.)  
-
-	$x = $cgi->param ("entr");
-	$entrs = unserialize ( $x );
-	foreach $entr ($entrs) {
-	    if (!$entr->{id}) {	# This is new entry. 
-		$entr->{stat} = $::KW->{STAT}{N}{id}; # Force entr.stat=New.
-		$entr->{seq} = undef;  # Force addentr() to assign seq number.
-		  # Prevent client from inserting extra bogus history.
-	        if (1 != scalar(@{$entr->{_hist}})) { die "Expected exactly 1 hist record"; } 
-	        $entr->{_hist}[0]{dt} = strftime ("%Y-%m-%d %H:%M:00-00", gmtime()); }
-	    else {
-		  # Reset seq, src, and history from original entr.
-		resethist ($dbh, $entr); }
-	        
-	    ($eid,$seq,$src) = addentr ($dbh, $entr); 
-	    $dbh->commit ();
-	    push (@added, [$eid,$seq,$src]); }
-	results_page (\@added);
+	$disp = $cgi->param ('disp')||"";  # '': User submission, 'a': Approve. 'r': Reject;
+	if (!is_editor () && $disp) {
+	    push (@errs, "Only registered editors can approve or reject entries"); }
+	if (!@errs) {
+	    $entrs = unserialize ($cgi->param ("entr"));
+	    foreach $entr ($entrs) {
+		push (@added, submission ($dbh, $svc, $entr, $disp, \@errs)); } }
+	if (!@errs) { 
+	    results_page (\@added, $svc);
+	    $dbh->commit (); }
+	else { 
+	    errors_page (\@errs, $svc);
+	    $dbh->rollback (); }
 	$dbh->disconnect; }
 
-    sub resethist { my ($dbh, $entr) = @_;
-	my ($submitter_hist, $sql, $recs);
-	if (1 > scalar(@{$entr->{_hist}})) { die ("No hist records"); } 
-	$submitter_hist = $entr->{_hist}[0];
-	$submitter_hist->{dt} = strftime ("%Y-%m-%d %H:%M:00-00", gmtime());  
-	$entr->{stat} = $::KW->{STAT}{M}{id}; # Force entr.stat=New.
-	$sql = "SELECT e.seq,e.src FROM entr e WHERE e.id=?";
-	$recs = dbread ($dbh, $sql, [$entr->{id}]);
-	if ((scalar(@$recs)) != 1) { die "Entry id $entr->{id} not found, unable to update\n"; }
+    sub submission { my ($dbh, $svc, $entr, $disp, $errs) = @_;
+	my (@added, $merge_rev, $added);
 
-	if ($entr->{seq} and $entr->{seq} != $recs->[0]{seq}) { 
-	    die "Submitted seq number $entr->{seq} does not match original seq number $recs->[0]{seq}\n" }
-	if ($entr->{src} and $entr->{src} != $recs->[0]{src}) { 
-	    die "Submitted src id $entr->{src} does not match original src id $recs->[0]{src}\n" }
-	
-	$sql = "SELECT h.* FROM hist h WHERE entr=? ORDER BY h.dt DESC";
-	$recs = dbread ($dbh, $sql, [$entr->{id}]);
-	unshift (@$recs, $submitter_hist); }
+	if (!$entr->{dfrm}) {	# This is new entry. 
+	    $entr->{STAT} = $::KW->{STAT}{N}{id};
+	    $entr->{id} = undef;
+	    $entr->{seq} = undef; }  # Force addentr() to assign seq number. 
+	else {			# Modification of existing entry.
+	    if ($entr->{stat} == $::KW->{STAT}{N}{id}) {
+		push (@$errs, "Bad entry, stat=$::KW->{STAT}{N}{kw}\n"); 
+		return; } 
+	    if ($entr->{stat} == $::KW->{STAT}{D}{id}) {
+		  # If this is a deletion, set $merge_rev.  When passed
+		  # to function merge_hist() it will tell it to return the 
+		  # edited entry's parent, rather than the edited entry
+		  # itself.  The reason is that if we are doing a delete,
+		  # we do not want to make any changes to the entry, even
+		  # if the submitter has done so. 
+		$merge_rev = 1; } }
 
-    sub results_page { my ($added) = @_;
-	my @m = map ("\n      <a href=\"entr.pl?q=$_->[1].$_->[2]\">$_->[1]</a>", @$added);
+	  # Merge_hist() will combine the history entry in the submitted
+	  # entry with the all the previous history records in the 
+	  # parent entry, so the the new entry will have a continuous
+	  # history.  In the process it checks that the parent entry
+	  # exists -- it might not if someone else has approved a 
+	  # different edit in the meantime.
+	if (!($entr = merge_hist ($dbh, $entr, $merge_rev))) {
+	    push (@$errs, 
+		"The entry you are editing has been changed by " .
+		"someone else.  Please check the current entry and " .
+		"reenter your changes if they are still applicable."); }
+
+	if (!@$errs) {
+	    if (!$disp) {
+		$entr->{unap} = 1;
+		$added = submit ($dbh, $svc, $entr, $errs); }
+	    elsif ($disp eq "a") {
+		$added = approve ($dbh, $svc, $entr, $errs); }
+	    elsif ($disp eq "r") {
+		$added = reject ($dbh, $svc, $entr, $errs); }
+	    else {
+		push (@$errs, "Bad url parameter (disp=$disp)"); } }
+	return $added; }
+
+    sub submit { my ($dbh, $svc, $entr, $errs) = @_;
+	my (@res);
+
+	if (!$entr->{dfrm} && $entr->{stat} != $::KW->{STAT}{N}{id}) { 
+	    push (@$errs, "Bad url parameter, no dfrm");  return; }
+	if ($entr->{stat} == $::KW->{STAT}{R}{id}) { 
+	    push (@$errs, "Bad url parameter, stat=R");  return; }
+	$entr->{unap} = 1;
+	@res = addentr ($dbh, $entr); 
+	return \@res; }
+
+    sub approve { my ($dbh, $svc, $entr, $errs) = @_;
+	my ($editor, $sql, $rs, @res, $dfrmid, $t, @t, $edroot);
+
+	$dfrmid = $entr->{dfrm};
+	if ($dfrmid) {
+	      # Since $dfrmid is not undef, this is an edit of an
+	      # existing entry.  We need to make sure there is a
+	      # single edit chain back to the root entry, i.e., 
+	      # there are no other pending edits which would get
+	      # discarded if we blindly apply our edit.
+	      # First, make sure the edit tree root still exists.  
+ 	    $sql = "SELECT * FROM find_edit_root(?)";
+	    $rs = dbread ($dbh, $sql, [$dfrmid]);
+	    $edroot = $rs->[0]{id};
+	    if (!$edroot) {
+		push (@$errs, 
+		    "The entry you are editing has been changed by " .
+		    "someone else.  Please check the current entry and " .
+		    "reenter your changes if they are still applicable."); }
+
+	      # Second, find all tree leaves.  These are the current 
+	      # pending edits.  If there is only one, it must be ours.
+	      # If there are more than one, then they need to be rejected
+	      # before the current entry can be approved. 
+	    $sql = "SELECT * FROM find_edit_leaves(?)";
+	    $rs = dbread ($dbh, $sql, [$edroot]);
+	    if (scalar (@$rs) > 1) {
+		$t = "<a href=\"entr.pl?svc=$svc&e=%s\">%s</a>";
+		@t = map (sprintf ($t, $_, $_), grep ($_!=$dfrmid, map ($_->{id}, @$rs)));
+		push (@$errs, 
+		    "There are other submitted edits (" . 
+		    join (", ", @t) . ") .  They must be " .
+		    "rejected before your edit can be approved.");
+		return; }
+
+	      # We may not find even our own edit if someone else rejected 
+	      # the edit we are working on.
+	    elsif ( scalar (@$rs) < 1) {
+		push (@$errs, 
+		    "The entry you are editing has been changed by " .
+		    "someone else.  Please check the current entry and " .
+		    "reenter your changes if they are still applicable.");
+		return; } }
+	  # Check stat.  May be N, A or D, but not R.  If it is N, 
+	  # change it to A.
+	if ($entr->{stat} == $::KW->{STAT}{N}{id}) { 
+	    $entr->{stat} = $::KW->{STAT}{A}{id}; }
+	if ($entr->{stat} == $::KW->{STAT}{R}{id}) {
+	    push (@$errs, "Bad url parameter, stat=R"); return; }  
+
+	  # The entr value for an approved, root entry and write it to
+	  # the database..
+	$entr->{dfrm} = undef;
+	$entr->{unap} = 0;
+	@res = addentr ($dbh, $entr);
+	  # Delete the old root if any.  Because the dfrm foreign key is
+	  # specified with "on delete cascade", deleting the root entry
+	  # will also delete all it's children. 
+	if ($edroot) { delentr ($dbh, $edroot); }
+	  # If we managed to do everything above without errors then
+	  # we can commit the changes and we're done.
+	$dbh->commit();
+	return \@res; }
+
+    sub reject { my ($dbh, $svc, $entr, $errs) = @_;
+	my ($sql, $rs, $chhead, @res);
+	  # Stored procedure 'find_chain_head()' will  follow the
+	  # dfrm chain from entr->{dfrm} back to it's head (the entry
+	  # immediately preceeding a non-chain entry.  A non-chain
+	  # entry is one with a NULL dfrm value or referenced (via
+	  # dfrm) by more than one other entry. 
+	$sql = "SELECT id FROM find_chain_head ($entr->{dfrm})";
+	$rs = dbread ($dbh, $sql);
+	$chhead = $rs->[0]{id};
+	if (!$chhead) {
+	    push (@$errs, 
+		"The entry you are editing has been changed by " .
+		"someone else.  Please check the current entry and " .
+		"reenter your changes if they are still applicable.");
+	    return; }
+	$entr->{stat} = $::KW->{STAT}{R}{id};
+	$entr->{dfrm} = undef;
+	$entr->{unap} = 0;
+	@res = addentr ($dbh, $entr);
+	delentr ($dbh, $chhead); 
+	$dbh->commit();
+	return \@res; }
+
+    sub merge_hist { my ($dbh, $entr, $rev) = @_;
+	# Merge the history from the derived-from entry
+	# having id $dfrmid, into the entry object $entr.
+	# Only the last hist entry of $entr is kept; all
+	# earlier entries are replaced with history entries
+	# from entry $dfrmid.  The timestamp in the last
+	# entry is reset to the current date/time.
+	# If $rev is true, then merge the new hist record
+	# into the orignal entry.  This is used when processing
+	# a delete entry where we want to ignore the parsed 
+	# entry and use the original entr.
+
+	my ($old, $hist, $newest);
+	$hist = [];
+	if ($entr->{dfrm}) {
+	    $old = EntrList ($dbh, "SELECT ?::INT AS id", [int($entr->{dfrm})]);
+	    if (scalar (@$old) != 1) { return 0; }
+	    $old = $old->[0];
+	    $hist = $old->{_hist}; }
+	$newest = pop (@{$entr->{_hist}});
+	$newest->{dt} = strftime ("%Y-%m-%d %H:%M:00-00", gmtime());
+	push (@$hist, $newest);
+	if (!$rev) {
+	    $entr->{_hist} = $hist; 
+	    return $entr; }
+	else {
+	    $old->{stat} = $entr->{stat};
+	    $old->{dfrm} = $entr->{dfrm};
+	    $old->{_hist} = $hist;
+	    return $old; } }
+
+    sub delentr { my ($dbh, $id) = @_;
+	# Delete entry $id (and by cascade, any edited entries
+	# based on this one).  This function deletes the entire
+	# entry, including history.  To delete the entry contents
+	# but leaving the entr and hist records, use database 
+	# function delentr.
+	my $sql = "DELETE FROM entr WHERE id=?";
+	$dbh->do ($sql, undef, $id); }
+
+    sub is_editor {
+	return 1; }
+
+    sub errors_page { my ($errs, $svc) = @_;
+        my $err_details = join ("\n    <br/>", @$errs);
+	print <<EOT;
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Invalid parameters</title>
+  </head>
+<body>
+  <h2>Submission data errors</h2>
+        Your submission cannot be processed due to the following errors:
+  <p>$err_details
+  </body>
+</html>
+EOT
+	}
+
+    sub results_page { my ($added, $svc) = @_;
+	$svc = $svc ? "svc=$svc&" : "";
+	my @m = map ("\n      <a href=\"entr.pl?${svc}q=$_->[1].$_->[2]\">$_->[1]</a>", @$added);
 	my $seqlnks = join ("    , " , @m);
 	print <<EOT;
 <html>
@@ -103,7 +376,7 @@ eval { binmode($DB::OUT, ":encoding(shift_jis)"); };
 <body>
   <h1>Entry Submitted</h1>
   <p/>Thank you for your contribution!
-  <p/>Your submission(s) have been added to the JMdict database
+  <p/>Your updates have been added to the JMdict database
     as provsional entries with the following seq. numbers:
   <p/>$seqlnks
   <p/>After review by the JMdict editors, they will be accepted

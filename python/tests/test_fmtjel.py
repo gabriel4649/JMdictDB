eb4d382925b702e59623d9d80400089fccabc50a
stuart <none@none>
2009-03-13 12:13:45 -0600

    Forgot to update fmtjel tests in last rev.

--- python/tests/test_fmtjel.py
+++ python/tests/test_fmtjel.py
@@ -103,7 +103,7 @@ class Test_restr (unittest.TestCase):
     def setUp (self): globalSetup ()
     def test_001(_): 
 	e1 = Entr (id=100, src=1, seq=1000010, stat=2, unap=False)
-	expect = 'jmdict 1000010 A {100}'
+	expect = 'jmdict 1000010 A {100}\n\n\n'
 	jeltxt = fmtjel.entr (e1)
 	_.assertEqual (expect, jeltxt)
     def test_002(_): 
@@ -116,7 +116,7 @@ class Test_restr (unittest.TestCase):
 	r = Restr(); e1._rdng[2]._restr.append (r); e1._kanj[1]._restr.append(r)
 	expect =  'jmdict 1000010 A {100}\n' \
 		  '手紙；切手\n' \
-		  'てがみ[restr=手紙]；あとで[restr=切手]；きって[restr=nokanji]' \
+		  'てがみ[restr=手紙]；あとで[restr=切手]；きって[restr=nokanji]\n' \
 		  .decode('utf-8')
 	jeltxt = fmtjel.entr (e1)
 	msg = "\nA:\n%s\nB:\n%s" % (expect, jeltxt)

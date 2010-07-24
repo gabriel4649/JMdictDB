f979ed717541e65e2474fcf5660c2e93f4beee8e
stuart <none@none>
2010-07-23 18:22:38 -0600

    Fix doc for database view get_subtree(), cycles not a problem.

--- pg/mkviews.sql
+++ pg/mkviews.sql
@@ -320,16 +320,11 @@ CREATE OR REPLACE FUNCTION delentr(entrid int) RETURNS void AS $$
 	END;
     $$ LANGUAGE plpgsql;
 
--------------------------------------------------------------
--- 
--------------------------------------------------------------
-
 CREATE OR REPLACE FUNCTION get_subtree (eid INT) RETURNS SETOF entr AS $$
     -- Return the set of entr rows that reference the row with id
     -- 'eid' via 'dfrm', and all the row that reference those rows
-    -- and...recursively.  Currently, if there is a cycle in the 
-    -- dfrm references, this function will fail to terminate.
-    -- FIXME: Detect cycles and abort.  
+    -- and so on.  This function will terminate even if there are
+    -- 'dfrm' cycles.
     BEGIN
 	RETURN QUERY
 	    WITH RECURSIVE wt(id) AS (
@@ -347,8 +342,9 @@ CREATE OR REPLACE FUNCTION get_edroot (eid INT) RETURNS SETOF int AS $$
     -- Starting at entry 'eid', follow the chain of 'dfrm' foreign
     -- keys until a entr row is found that has a NULL 'dfrm' value,
     -- and return that row (which may be the row with id of 'eid').
-    -- If there is no row with an id of 'eid', a NULL row (one with
-    -- every attribute set to NULL) is returned. 
+    -- If there is no row with an id of 'eid', or if there is a cycle
+    -- in the dfrm references such that none of entries have a NULL
+    -- dfrm, no rows are returned. 
     BEGIN
 	RETURN QUERY
 	    WITH RECURSIVE wt(id,dfrm) AS (

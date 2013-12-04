acc00ad2a6a16fef98135be69df1035c7cfd363b
stuart <none@none>
2013-12-03 23:12:40 -0700

    conj: fix patch file.

--- patches/010.sql
+++ patches/010.sql
@@ -2,9 +2,18 @@
 -- Trans: 9->10
 
 \set ON_ERROR_STOP
-BEGIN;
+--BEGIN;  -- Note that because conj.sql and loadconj.sql have
+          -- their own BEGIN and COMMIT statements, we can't 
+          -- wrap them in our own enclosing transation since 
+          -- pg doesn't support nested transactions.  If there 
+          -- is an error, you'll need to manually fix things
+          -- (delete the conj* tables and reset he patchlevel
+          -- number back to 9 before you run this again.)
 
-\i pg/loadconj.sql
+\encoding utf-8
+\cd pg
+\i conj.sql
+\i loadconj.sql
 
 INSERT INTO dbpatch(level) VALUES(10);
-COMMIT;
+--COMMIT;

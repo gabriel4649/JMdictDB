aa52ea03e647fdc88438f83dd4d8f6880b6088db
stuart <none@none>
2018-04-06 11:38:59 -0600

    logger.py: fix fail to write non-ascii message to logfile
    When Python logging module used in a cgi script (at least in the case of
    Apache-2.4, not sure if this is server dependent) to write mesasges to a
    logfile, a message with non-ascii characters results in a UnicodeEncodeError
    exception which is handled by the logging module code by writing the error
    to stderr (causing it to end up in the webserver's error log file).  The
    orginal message is dropped and never appears in the given logfile.
    The workaround seems to be to explicitly open the log file with utf-8
    encoding so the UnicodeEncodeError doesn't happen.
    
    Also, check if the log file is writable; if not writable (because it
    doesn't exist, permissions, etc) write a log message to stderr and
    disable further logging.  The intent is to provide notice that the
    logging file is inaccessible without generating such messages on every
    attempt to wtite a logging message (although they will be generated
    for every cgi script excecution which might be worse.)

--- python/lib/logger.py
+++ python/lib/logger.py
@@ -20,22 +20,41 @@
 # Provide a standard logging configuration for JMdictDB cgi scripts, 
 # tools and library functions.
 
-import sys, logging, traceback, os, datetime; L = logging.getLogger;
-import jmcgi
+import sys, logging, traceback, os, datetime
+
+L = logging.getLogger;
 
 def log_config (level="debug", filename=None):
         """
         level -- One of: "critical", "error", "warning", "info", "debug".
         filename -- If not given or None, logging messages will be written 
           to sys.stderr.  Otherwise the name of a file to which the logging
-          messages will be appended. 
+          messages will be appended.  If the file is not writable at the 
+          time this function is called, a logging message will be written
+          to stderr to that effect and further logging disabled.
         """
 
-        if filename and os.access (filename, os.W_OK):
-            msgdest = {'filename': filename }
-        else: msgdest = {'stream': sys.stderr}
+        msgdest, disable = {'stream': sys.stderr}, False
+        if filename:
+            if os.access (filename, os.W_OK):
+                  # We should be able to set 'msgdest' below to simply:
+                  #   {'filename': filename}
+                  # However, it seems that when running as a cgi script under
+                  # Apache-2.4, the file is opened with ascii encoding rather
+                  # than utf-8.  When a logging message is written that contains
+                  # non-ascii characters, a UnicodeEncodingError is raised.
+                  # Curiously, the error and traceback is written to the web
+                  # server log file and then control is returned to the python
+                  # program that generated the message, which contrinues on 
+                  # normally, though the original non-ascii log message never
+                  # appears in the logging log file.  The following forces the
+                  # logging file to be opened with utf-8 encoding.
+                msgdest = {'handlers':
+                           [logging.FileHandler(filename,'a','utf-8')] }
+            else: disable = True
         lvl = logging.getLevelName (level.upper())
-        if not isinstance (lvl, int): raise ValueError ("bad 'level' parameter: %s" % level)
+        if not isinstance (lvl, int):
+            raise ValueError ("bad 'level' parameter: %s" % level)
         logging.basicConfig (
             level=lvl,
             format='%(asctime)s %(levelname)1.1s [%(process)d] %(name)s: %(message)s',
@@ -43,9 +62,12 @@ def log_config (level="debug", filename=None):
               # When both "stream" and "filename" are present and non-None, 
               # "filename" takes precedence according to 
             **msgdest)
+        if disable:
+            L('logger').error('Unable to write to logging file: %s' % filename)
+            logging.disable (logging.CRITICAL)
 
 def handler( ex_cls, ex, tb ):
-          # 'errid' not used yet but will be in future.
+        import jmcgi
         errid = datetime.datetime.now().strftime("%y%m%d-%H%M%S")\
                 + '-' + str(os.getpid())
         logging.critical( '\n{0}: {1}'.format(ex_cls, ex) )
